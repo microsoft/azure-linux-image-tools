97a3fb69d4b1541c5ff0ffcdcc29693bbd53f3ff
Chris Gunn <chrisgun@microsoft.com>
2024-12-11 16:45:13 -0800

    VM Tests: Run Image Customizer container. (#21)
    Add executing the Image Customizer container in Docker to the basic
    test.
    
    The test suite's Makefile will call the toolkit's Makefile to
    automatically rebuild the imagecustomizer binary and the container. This
    avoids the user needing to provide a URL to the container. And it
    ensures the tests are always running against the code within the
    checked-out repo.

--- test/vmtests/vmtests/test_no_change.py
+++ test/vmtests/vmtests/test_no_change.py
@@ -1,6 +1,28 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
+from pathlib import Path
 
-def test_no_change() -> None:
-    pass
+from docker import DockerClient
+
+from .conftest import TEST_CONFIGS_DIR
+from .utils.imagecustomizer import run_image_customizer
+
+
+def test_no_change(
+    docker_client: DockerClient,
+    image_customizer_container_url: str,
+    core_efi_azl2: Path,
+    test_temp_dir: Path,
+) -> None:
+    config_path = TEST_CONFIGS_DIR.joinpath("nochange-config.yaml")
+    output_image_path = test_temp_dir.joinpath("image.qcow2")
+
+    run_image_customizer(
+        docker_client,
+        image_customizer_container_url,
+        core_efi_azl2,
+        config_path,
+        "qcow2",
+        output_image_path,
+    )
