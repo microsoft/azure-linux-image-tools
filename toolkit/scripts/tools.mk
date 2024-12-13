c2fe330232f14fc33d13f380c61411958d4b4b14
Chris Gunn <chrisgun@microsoft.com>
2024-12-12 16:56:20 -0800

    Delete unneeded toolkit code.
    1. Remove all the Go tools that aren't needed.
    
    2. Remove all the code in the Makefile that isn't needed.
    
    Note: This change intentionally doesn't try to refactor anything.
    That can be done in a subsequent change.

--- toolkit/scripts/tools.mk
+++ toolkit/scripts/tools.mk
@@ -4,9 +4,6 @@
 # Contains:
 #	- Go Utilities
 
-$(call create_folder,$(RPMS_DIR))
-$(call create_folder,$(CACHED_RPMS_DIR)/cache)
-$(call create_folder,$(CCACHE_DIR))
 $(call create_folder,$(TOOL_BINS_DIR))
 $(call create_folder,$(BUILD_DIR)/tools)
 
@@ -29,44 +26,22 @@ endif
 
 # List of go utilities in tools/ directory
 go_tool_list = \
-	bldtracker \
-	boilerplate \
-	containercheck \
-	depsearch \
-	downloader \
-	grapher \
-	graphpkgfetcher \
-	graphanalytics \
-	graphPreprocessor \
 	imageconfigvalidator \
 	imagecustomizer \
-	imagepkgfetcher \
 	imager \
 	isomaker \
-	licensecheck \
 	liveinstaller \
 	osmodifier \
-	pkgworker \
-	precacher \
-	repoquerywrapper \
 	roast \
-	rpmssnapshot \
-	scheduler \
-	specarchchecker \
-	specreader \
-	srpmpacker \
-	validatechroot \
 
 # For each utility "util", create a "out/tools/util" target which references code in "tools/util/"
 go_tool_targets = $(foreach target,$(go_tool_list),$(TOOL_BINS_DIR)/$(target))
 # Common files to monitor for all go targets
 go_module_files = $(TOOLS_DIR)/go.mod $(TOOLS_DIR)/go.sum
 go_internal_files = $(shell find $(TOOLS_DIR)/internal/ -type f -name '*.go')
-go_grapher_files = $(shell find $(TOOLS_DIR)/grapher/ -type f -name '*.go')
 go_pkg_files = $(shell find $(TOOLS_DIR)/pkg/ -type f -name '*.go')
 go_imagegen_files = $(shell find $(TOOLS_DIR)/imagegen/ -type f -name '*.go')
-go_scheduler_files = $(shell find $(TOOLS_DIR)/scheduler -type f -name '*.go')
-go_common_files = $(go_module_files) $(go_internal_files) $(go_grapher_files) $(go_imagegen_files) $(go_pkg_files) $(go_scheduler_files) $(STATUS_FLAGS_DIR)/got_go_deps.flag $(BUILD_DIR)/tools/internal.test_coverage
+go_common_files = $(go_module_files) $(go_internal_files) $(go_imagegen_files) $(go_pkg_files) $(STATUS_FLAGS_DIR)/got_go_deps.flag $(BUILD_DIR)/tools/internal.test_coverage
 # A report on test coverage for all the go tools
 test_coverage_report=$(TOOL_BINS_DIR)/test_coverage_report.html
 
