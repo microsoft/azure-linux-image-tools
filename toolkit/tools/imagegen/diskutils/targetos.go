7f40d2cdb740d87320b7edbb1b18e3c754b76535
Chris Gunn <chrisgun@microsoft.com>
2024-12-17 15:21:45 -0800

    WIP

--- toolkit/tools/imagegen/diskutils/targetos.go
+++ toolkit/tools/imagegen/diskutils/targetos.go
@@ -3,9 +3,23 @@
 
 package diskutils
 
+import (
+	"path/filepath"
+	"strings"
+
+	"github.com/microsoft/azurelinux/toolkit/tools/internal/file"
+)
+
 type TargetOs string
 
 const (
 	TargetOsAzureLinux2 TargetOs = "azl2"
 	TargetOsAzureLinux3 TargetOs = "azl3"
 )
+
+func GetInstalledTargetOs(rootfs string) (TargetOs, error) {
+	osReleaseLines, err := file.ReadLines(filepath.Join(rootfs, "etc/os-release"))
+	for _, line := range osReleaseLines {
+		strings.SplitN(line, "=", 1)
+	}
+}
