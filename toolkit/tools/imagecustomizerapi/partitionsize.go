563d12f585ca54651cbb0f4c1ae4bf62d9f41341
Amrita Kohli <amritakohli@microsoft.com>
2024-12-17 21:51:58 +0000

    partition size handling

--- toolkit/tools/imagecustomizerapi/partitionsize.go
+++ toolkit/tools/imagecustomizerapi/partitionsize.go
@@ -4,6 +4,7 @@
 package imagecustomizerapi
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"gopkg.in/yaml.v3"
@@ -22,8 +23,8 @@ const (
 )
 
 type PartitionSize struct {
-	Type PartitionSizeType
-	Size DiskSize
+	Type PartitionSizeType `json:"type,omitempty"`
+	Size DiskSize          `json:"size,omitempty"`
 }
 
 func (s *PartitionSize) IsValid() error {
@@ -43,6 +44,50 @@ func (s *PartitionSize) UnmarshalYAML(value *yaml.Node) error {
 	case PartitionSizeGrow:
 		*s = PartitionSize{
 			Type: PartitionSizeTypeGrow,
+		}
+		return nil
+	}
+
+	diskSize, err := parseDiskSize(stringValue)
+	if err != nil {
+		return fmt.Errorf("%w:\nexpected format: grow | <NUM>(K|M|G|T) (e.g. grow, 100M, 1G)", err)
+	}
+
+	*s = PartitionSize{
+		Type: PartitionSizeTypeExplicit,
+		Size: diskSize,
+	}
+	return nil
+}
+
+func (s PartitionSize) MarshalJSON() ([]byte, error) {
+	switch s.Type {
+	case PartitionSizeTypeGrow:
+		return json.Marshal("grow")
+	case PartitionSizeTypeExplicit:
+		return json.Marshal(s.Size.String())
+	default:
+		return json.Marshal(nil)
+	}
+}
+
+func (s *PartitionSize) UnmarshalJSON(data []byte) error {
+	var err error
+	var stringValue string
+	err = json.Unmarshal(data, &stringValue)
+	if err != nil {
+		return fmt.Errorf("invalid partition size format: %w", err)
+	}
+
+	switch stringValue {
+	case PartitionSizeGrow:
+		*s = PartitionSize{
+			Type: PartitionSizeTypeGrow,
+		}
+		return nil
+	case "":
+		*s = PartitionSize{
+			Type: PartitionSizeTypeUnset,
 		}
 		return nil
 	}
