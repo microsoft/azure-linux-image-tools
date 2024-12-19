2df524041bfb27ea250b850fbaa90710971d1ccd
Amrita Kohli <amritakohli@microsoft.com>
2024-12-19 00:46:12 +0000

    octal

--- toolkit/tools/pkg/imagecustomizerlib/imagehistory.go
+++ toolkit/tools/pkg/imagecustomizerlib/imagehistory.go
@@ -43,7 +43,7 @@ func addImageHistory(imageChroot *safechroot.Chroot, imageUuid string, baseConfi
 	}
 
 	customizerLoggingDirPath := filepath.Join(imageChroot.RootDir(), customizerLoggingDir)
-	err = os.MkdirAll(customizerLoggingDirPath, 0755)
+	err = os.MkdirAll(customizerLoggingDirPath, 0o755)
 	if err != nil {
 		return fmt.Errorf("failed to create customizer logging directory: %w", err)
 	}
