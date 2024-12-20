ad4f29e4cac759948b67a291ad44973ef113736c
Chris Gunn <chrisgun@microsoft.com>
2024-12-20 15:40:23 -0800

    Feedback updates.

--- toolkit/tools/imagecustomizerapi/partitiontype_arm64.go
+++ toolkit/tools/imagecustomizerapi/partitiontype_arm64.go
@@ -4,6 +4,9 @@
 package imagecustomizerapi
 
 var (
+	// UUUIs come from:
+	// - https://en.wikipedia.org/wiki/GUID_Partition_Table#Partition_type_GUIDs
+	// - https://uapi-group.org/specifications/specs/discoverable_partitions_specification/
 	partitionTypeToUuidArchDependent = map[PartitionType]string{
 		PartitionTypeRoot:       "b921b045-1df0-41c3-af44-4c6f280d3fae",
 		PartitionTypeRootVerity: "df3300ce-d69f-4c92-978c-9bfb0f38d820",
