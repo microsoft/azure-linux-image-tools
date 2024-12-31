a7e35dd69c2d6448cc1ab2922953008ed1a30007
Roaa Sakr <romoh@microsoft.com>
2024-12-31 11:59:44 -0800

    jsonschema cli
    Signed-off-by: Roaa Sakr <romoh@microsoft.com>

---
+++ toolkit/tools/jsonschemacli/main.go
@@ -0,0 +1,38 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/invopop/jsonschema"
+	"github.com/microsoft/azurelinux/toolkit/tools/imagecustomizerapi"
+)
+
+func main() {
+	if len(os.Args) < 1 {
+		fmt.Println("Usage: jsonschemacli <output_file>")
+		os.Exit(1)
+	}
+
+	reflector := jsonschema.Reflector{}
+
+	schema := reflector.Reflect(&imagecustomizerapi.Config{})
+
+	schemaJSON, err := json.MarshalIndent(schema, "", "  ")
+	if err != nil {
+		log.Fatalf("Failed to marshal schema: %v", err)
+	}
+
+	outputFile := os.Args[1]
+	err = os.WriteFile(outputFile, schemaJSON, 0644)
+	if err != nil {
+		log.Fatalf("Failed to write schema to file: %v", err)
+	}
+
+	fmt.Printf("JSON schema has been written to %s\n", outputFile)
+}
