10196880d3bfcf14f5e01dee3fd35e7b93047f58
George Mileka <gmileka@outlook.com>
2025-01-06 18:29:12 -0800

    Remove debug messages.

--- toolkit/tools/imagecustomizer/container/test-mic-container.sh
+++ toolkit/tools/imagecustomizer/container/test-mic-container.sh
@@ -34,5 +34,4 @@ docker run --rm \
         --config-file "$containerInputConfig" \
         --build-dir "$containerBuildDir" \
         --output-image-format "vhdx" \
-        --output-image-file "$containerOutputImage" \
-        --log-level debug
+        --output-image-file "$containerOutputImage"
