microsoft/azure-linux-image-tools

commit db361b264064751a266907eb028f84636bc052dc

Add multi-kernel support. (#109)
Remove the arbitary restrictions to allow the kernel args to be updated
when multiple kernels are installed.

In addition, remove the logic that sets `"GRUB_DISABLE_RECOVERY"` when
verity is enabled, since the logic can now handle multiple boot entries
in the grub.cfg file when a single kernel is installed.

Also, remove the requirement for `$kernelopts` to exist in the grub.cfg
file, since this logic just complicates the code.

toolkit/tools/pkg/imagecustomizerlib/grubcfgutils.go
@@ -240,8 +240,8 @@ func setInitrdPath(inputGrubCfgContent string, initrdPath string) (outputGrubCfg
 // If $kernelopts is present, extraCommandLine is inserted before $kernelopts.
 // If $kernelopts is not present, extraCommandLine is appended at the end.
 func appendKernelCommandLineArgsAll(inputGrubCfgContent string, extraCommandLine string,
-	allowMultiple bool, requireKernelOpts bool) (outputGrubCfgContent string, err error) {
-	lines, err := findLinuxOrInitrdLineAll(inputGrubCfgContent, linuxCommand, allowMultiple)
+) (outputGrubCfgContent string, err error) {
+	lines, err := findLinuxOrInitrdLineAll(inputGrubCfgContent, linuxCommand, true /*allowMultiple*/)
 	if err != nil {
 		return "", err
 	}
@@ -256,26 +256,15 @@ func appendKernelCommandLineArgsAll(inputGrubCfgContent string, extraCommandLine
 		// Skip the "linux" command and the kernel binary path arg.
 		argTokens := line.Tokens[2:]
 
-		insertAt, err := findCommandLineInsertAt(argTokens, requireKernelOpts)
+		insertAt, err := findCommandLineInsertAt(argTokens, line.Tokens[1].Loc.End.Index)
 		if err != nil {
 			return "", err
 		}
 
-		leadingSpace := " "
-		if requireKernelOpts {
-			// When requireKernelOpts is true, we are inserting right before
-			// kernelOpts, and there is already an empty space.
-			leadingSpace = ""
-		}
-		outputGrubCfgContent = outputGrubCfgContent[:insertAt] + leadingSpace + extraCommandLine + " " + outputGrubCfgContent[insertAt:]
+		outputGrubCfgContent = outputGrubCfgContent[:insertAt] + " " + extraCommandLine + " " + outputGrubCfgContent[insertAt:]
 	}
 
 	return outputGrubCfgContent, nil
-}
-
-// Appends kernel command-line args to the linux command within a grub config file.
-func appendKernelCommandLineArgs(inputGrubCfgContent string, extraCommandLine string) (outputGrubCfgContent string, err error) {
-	return appendKernelCommandLineArgsAll(inputGrubCfgContent, extraCommandLine, false /*allow multiple*/, true /*requireKernelOpts*/)
 }
 
 type grubConfigLinuxArg struct {
@@ -295,16 +284,17 @@ type grubConfigLinuxArg struct {
 //   - args: A list of kernel command-line arguments.
 //   - insertAt: An index that represents an appropriate insert point for any new args.
 //     For Azure Linux 2.0 images, this points to the index of the $kernelopts token.
-func getLinuxCommandLineArgs(grub2Config string, requireKernelOpts bool) ([]grubConfigLinuxArg, int, error) {
-	linuxLine, err := findLinuxOrInitrdLineAll(grub2Config, linuxCommand, false /*allowMultiple*/)
+func getLinuxCommandLineArgs(grub2Config string) ([]grubConfigLinuxArg, int, error) {
+	linuxLines, err := findLinuxOrInitrdLineAll(grub2Config, linuxCommand, false /*allowMultiple*/)
 	if err != nil {
 		return nil, 0, err
 	}
 
 	// Skip the "linux" command and the kernel binary path arg.
-	argTokens := linuxLine[0].Tokens[2:]
+	linuxLine := linuxLines[0]
+	argTokens := linuxLine.Tokens[2:]
 
-	insertAt, err := findCommandLineInsertAt(argTokens, requireKernelOpts)
+	insertAt, err := findCommandLineInsertAt(argTokens, linuxLine.Tokens[1].Loc.End.Index)
 	if err != nil {
 		return nil, 0, err
 	}
@@ -319,10 +309,12 @@ func getLinuxCommandLineArgs(grub2Config string, requireKernelOpts bool) ([]grub
 
 // Takes a tokenized grub.cfg file and looks for an appropriate place to insert new args.
 // If $kernelopts is present, it returns its location for insertion.
-// If $kernelopts is absent,
-// - If requireKernelOpts is true, it fails (could not find required $kernelopts).
-// - If requireKernelOpts is false, it returns the location after the last token.
-func findCommandLineInsertAt(argTokens []grub.Token, requireKernelOpts bool) (int, error) {
+// If $kernelopts is absent, it returns the location after the last token.
+func findCommandLineInsertAt(argTokens []grub.Token, defaultValue int) (int, error) {
+	if len(argTokens) <= 0 {
+		return defaultValue, nil
+	}
+
 	insertAtTokens := []grub.Token(nil)
 	for i := range argTokens {
 		argToken := argTokens[i]
@@ -341,13 +333,9 @@ func findCommandLineInsertAt(argTokens []grub.Token, requireKernelOpts bool) (in
 	}
 
 	if len(insertAtTokens) < 1 {
-		// Could not find the grubKernelOpts
-		if !requireKernelOpts && len(argTokens) > 0 {
-			// Try to insert at the very end as long as there are other tokens.
-			return argTokens[len(argTokens)-1].Loc.End.Index, nil
-		} else {
-			return 0, fmt.Errorf("failed to find $%s in linux command line", grubKernelOpts)
-		}
+		// Could not find the grubKernelOpts.
+		// So, insert at the end.
+		return argTokens[len(argTokens)-1].Loc.End.Index, nil
 	}
 	if len(insertAtTokens) > 1 {
 		return 0, fmt.Errorf("too many $%s tokens found in linux command line", grubKernelOpts)
@@ -444,14 +432,14 @@ func findKernelCommandLineArgValue(args []grubConfigLinuxArg, name string) (stri
 	return lastArg.Value, nil
 }
 
-func replaceKernelCommandLineArgValueAll(inputGrubCfgContent string, name string, value string, allowMultiple bool,
-) (outputGrubCfgContent string, oldValues []string, err error) {
+func replaceKernelCommandLineArgValueAll(inputGrubCfgContent string, name string, value string,
+) (outputGrubCfgContent string, err error) {
 	newArg := fmt.Sprintf("%s=%s", name, value)
 	quotedNewArg := grub.QuoteString(newArg)
 
-	lines, err := findLinuxOrInitrdLineAll(inputGrubCfgContent, linuxCommand, allowMultiple)
+	lines, err := findLinuxOrInitrdLineAll(inputGrubCfgContent, linuxCommand, true /*allowMultiple*/)
 	if err != nil {
-		return "", nil, err
+		return "", err
 	}
 
 	outputGrubCfgContent = inputGrubCfgContent
@@ -466,31 +454,29 @@ func replaceKernelCommandLineArgValueAll(inputGrubCfgContent string, name string
 
 		args, err := ParseCommandLineArgs(argTokens)
 		if err != nil {
-			return "", nil, err
+			return "", err
 		}
 
 		foundArgs := findMatchingCommandLineArgs(args, []string{name})
 		if len(foundArgs) < 1 {
-			return "", nil, fmt.Errorf("failed to find kernel arg (%s)", name)
+			return "", fmt.Errorf("failed to find kernel arg (%s)", name)
 		}
 		if len(foundArgs) > 1 {
-			return "", nil, fmt.Errorf("too many instances of kernel arg found (%s)", name)
+			return "", fmt.Errorf("too many instances of kernel arg found (%s)", name)
 		}
 
 		arg := foundArgs[0]
 		start := arg.Token.Loc.Start.Index
 		end := arg.Token.Loc.End.Index
 
-		oldValues = append(oldValues, inputGrubCfgContent[start:end])
 		outputGrubCfgContent = outputGrubCfgContent[:start] + quotedNewArg + outputGrubCfgContent[end:]
 	}
 
-	return outputGrubCfgContent, oldValues, nil
+	return outputGrubCfgContent, nil
 }
 
-func updateKernelCommandLineArgsAll(grub2Config string, argsToRemove []string, newArgs []string,
-	allowMultiple bool, requireKernelOpts bool) (string, error) {
-	lines, err := findLinuxOrInitrdLineAll(grub2Config, linuxCommand, allowMultiple /*allowMultiple*/)
+func updateKernelCommandLineArgsAll(grub2Config string, argsToRemove []string, newArgs []string) (string, error) {
+	lines, err := findLinuxOrInitrdLineAll(grub2Config, linuxCommand, true /*allowMultiple*/)
 	if err != nil {
 		return "", err
 	}
@@ -504,7 +490,7 @@ func updateKernelCommandLineArgsAll(grub2Config string, argsToRemove []string, n
 		// Skip the "linux" command and the kernel binary path arg.
 		argTokens := line.Tokens[2:]
 
-		insertAtToken, err := findCommandLineInsertAt(argTokens, requireKernelOpts)
+		insertAtToken, err := findCommandLineInsertAt(argTokens, line.Tokens[1].Loc.End.Index)
 		if err != nil {
 			return "", err
 		}
@@ -520,19 +506,6 @@ func updateKernelCommandLineArgsAll(grub2Config string, argsToRemove []string, n
 		}
 	}
 	return grub2Config, nil
-}
-
-// Finds all the kernel command-line args that match the provided names, then insert replacement arg(s).
-//
-// Params:
-// - grub2Config: The string contents of the grub.cfg file.
-// - argsToRemove: A list of arg names to remove from the command-line args.
-// - newArgs: A list of new arg values to add to the command-line args.
-//
-// Output:
-// - grub2Config: The new string contents of the grub.cfg file.
-func updateKernelCommandLineArgs(grub2Config string, argsToRemove []string, newArgs []string) (string, error) {
-	return updateKernelCommandLineArgsAll(grub2Config, argsToRemove, newArgs, false /*allowMultiple*/, true /*requireKernelOpts*/)
 }
 
 func updateKernelCommandLineArgsHelper(value string, args []grubConfigLinuxArg, insertAt int,
@@ -625,13 +598,13 @@ func selinuxModeToArgsWithPermissiveFlag(selinuxMode imagecustomizerapi.SELinuxM
 }
 
 // Update the SELinux kernel command-line args.
-func updateSELinuxCommandLineHelperAll(grub2Config string, selinuxMode imagecustomizerapi.SELinuxMode, allowMultiple bool, requireKernelOpts bool) (string, error) {
+func updateSELinuxCommandLineHelperAll(grub2Config string, selinuxMode imagecustomizerapi.SELinuxMode) (string, error) {
 	newSELinuxArgs, err := selinuxModeToArgs(selinuxMode)
 	if err != nil {
 		return "", err
 	}
 
-	grub2Config, err = updateKernelCommandLineArgsAll(grub2Config, selinuxArgNames, newSELinuxArgs, allowMultiple, requireKernelOpts)
+	grub2Config, err = updateKernelCommandLineArgsAll(grub2Config, selinuxArgNames, newSELinuxArgs)
 	if err != nil {
 		return "", err
 	}

toolkit/tools/pkg/imagecustomizerlib/partitionutils.go
@@ -488,7 +488,7 @@ func extractKernelCmdlineFromGrub(bootPartition *diskutils.PartitionInfo,
 		return nil, fmt.Errorf("failed to read grub.cfg:\n%w", err)
 	}
 
-	args, _, err := getLinuxCommandLineArgs(string(grubCfgContent), true)
+	args, _, err := getLinuxCommandLineArgs(string(grubCfgContent))
 	if err != nil {
 		return nil, fmt.Errorf("failed to extract kernel command-line arguments: %w", err)
 	}

toolkit/tools/pkg/imagecustomizerlib/bootcustomizer.go
@@ -65,7 +65,7 @@ func (b *BootCustomizer) AddKernelCommandLine(extraCommandLine []string) error {
 		b.defaultGrubFileContent = defaultGrubFileContent
 	} else {
 		// Add the args directly to the /boot/grub2/grub.cfg file.
-		grubCfgContent, err := appendKernelCommandLineArgs(b.grubCfgContent, combinedArgs)
+		grubCfgContent, err := appendKernelCommandLineArgsAll(b.grubCfgContent, combinedArgs)
 		if err != nil {
 			return err
 		}
@@ -88,7 +88,7 @@ func (b *BootCustomizer) getSELinuxModeFromGrub() (imagecustomizerapi.SELinuxMod
 			return "", err
 		}
 	} else {
-		args, _, err = getLinuxCommandLineArgs(b.grubCfgContent, true /*requireKernelOpts*/)
+		args, _, err = getLinuxCommandLineArgs(b.grubCfgContent)
 		if err != nil {
 			return imagecustomizerapi.SELinuxModeDefault, err
 		}
@@ -163,7 +163,7 @@ func (b *BootCustomizer) UpdateKernelCommandLineArgs(defaultGrubFileVarName defa
 
 		b.defaultGrubFileContent = defaultGrubFileContent
 	} else {
-		grubCfgContent, err := updateKernelCommandLineArgs(b.grubCfgContent, argsToRemove, newArgs)
+		grubCfgContent, err := updateKernelCommandLineArgsAll(b.grubCfgContent, argsToRemove, newArgs)
 		if err != nil {
 			return err
 		}
@@ -184,34 +184,10 @@ func (b *BootCustomizer) PrepareForVerity() error {
 			return err
 		}
 
-		// Disable recovery menu entry, to avoid having more than 1 linux command in the grub.cfg file.
-		// This will make it easier to modify the grub.cfg file to add the verity args.
-		defaultGrubFileContent, err = UpdateDefaultGrubFileVariable(defaultGrubFileContent, "GRUB_DISABLE_RECOVERY",
-			"true")
-		if err != nil {
-			return err
-		}
-
 		// For rootfs verity, the root device will always be "/dev/mapper/root"
 		rootDevicePath := verityDevicePathFromName(imagecustomizerapi.VerityRootDeviceName)
 		defaultGrubFileContent, err = UpdateDefaultGrubFileVariable(defaultGrubFileContent, "GRUB_DEVICE",
 			rootDevicePath)
-		if err != nil {
-			return err
-		}
-
-		b.defaultGrubFileContent = defaultGrubFileContent
-	}
-
-	return nil
-}
-
-func (b *BootCustomizer) PrepareForUsrVerity() error {
-	if b.isGrubMkconfig {
-		// Disable recovery menu entry, to avoid having more than 1 linux command in the grub.cfg file.
-		// This will make it easier to modify the grub.cfg file to add the verity args.
-		defaultGrubFileContent, err := UpdateDefaultGrubFileVariable(b.defaultGrubFileContent, "GRUB_DISABLE_RECOVERY",
-			"true")
 		if err != nil {
 			return err
 		}

commit 48acae58b8f3ef9f4b0be80dcd36706628870f6e

Fix extra command line args in UKIs.
When generating a UKI, the extra commnand line args are missing from
the UKI. This was caused by the change (#109) that stopped setting
`GRUB_DISABLE_RECOVERY` in the `/etc/default/grub` file. That change
means there are now "recovery mode" entries for each kernel in the
`grub.cfg` file. So, when extracting the kernel args for UKI, we need
to ensure we ignore the "recovery mode" entries.

In addition, add a test that verifies the behavior of `/usr` verity
+ UKI. This includes verifying the extra command-line args are present
in the UKI.

Also, update the existing verity tests so that they also check that the
extra command-line args are present (in the grub.cfg file).

toolkit/tools/pkg/imagecustomizerlib/customizeuki.go
@@ -6,11 +6,11 @@ package imagecustomizerlib
 import (
 	"encoding/json"
 	"fmt"
-	"gopkg.in/ini.v1"
 	"os"
 	"path/filepath"
-	"regexp"
 	"strings"
+
+	"gopkg.in/ini.v1"
 
 	"github.com/microsoft/azurelinux/toolkit/tools/imagecustomizerapi"
 	"github.com/microsoft/azurelinux/toolkit/tools/imagegen/diskutils"
@@ -359,23 +359,38 @@ func extractKernelToArgsFromGrub(grubCfgPath string) (map[string]string, error) 
 		return nil, fmt.Errorf("failed to read grub.cfg file at (%s):\n%w", grubCfgPath, err)
 	}
 
-	linuxLineRegex := regexp.MustCompile(`^linux\s+(/vmlinuz-[^\s]+)\s+(.*)`)
-
-	lines := strings.Split(string(grubCfgContent), "\n")
-	kernelToArgs := make(map[string]string)
-
-	for _, line := range lines {
-		trimmedLine := strings.TrimSpace(line)
-		matches := linuxLineRegex.FindStringSubmatch(trimmedLine)
-		if len(matches) == 3 {
-			kernel := strings.TrimPrefix(matches[1], "/")
-			args := matches[2]
-			kernelToArgs[kernel] = strings.TrimSpace(args)
-		}
+	lines, err := FindNonRecoveryLinuxLine(grubCfgContent)
+	if err != nil {
+		return nil, fmt.Errorf("failed to find linux command lines in grub.cfg:\n%w", err)
 	}
 
-	if len(kernelToArgs) == 0 {
-		return nil, fmt.Errorf("failed to find any valid 'linux /vmlinuz-*' lines in grub.cfg file at (%s)", grubCfgPath)
+	kernelToArgs := make(map[string]string)
+	for _, line := range lines {
+		if len(line.Tokens) < 3 {
+			return nil, fmt.Errorf("linux line in grub.cfg file has less than 3 args")
+		}
+
+		kernel := line.Tokens[1].RawContent
+		kernel = strings.TrimPrefix(kernel, "/")
+
+		argTokens := line.Tokens[2:]
+		args, err := ParseCommandLineArgs(argTokens)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse linux command lines args (%s):\n%w", kernel, err)
+		}
+
+		filteredArgs := []string(nil)
+		for _, arg := range args {
+			if arg.ValueHasVarExpansion {
+				// Ignore tokens with $ vars.
+				continue
+			}
+
+			filteredArgs = append(filteredArgs, arg.Arg)
+		}
+
+		filteredArgsString := GrubArgsToString(filteredArgs)
+		kernelToArgs[kernel] = filteredArgsString
 	}
 
 	return kernelToArgs, nil

toolkit/tools/pkg/imagecustomizerlib/grubcfgutils.go
@@ -270,6 +270,8 @@ func appendKernelCommandLineArgsAll(inputGrubCfgContent string, extraCommandLine
 type grubConfigLinuxArg struct {
 	// The tokenizer token for the arg.
 	Token grub.Token
+	// The full arg string.
+	Arg string
 	// The name of the argument.
 	Name string
 	// The value of the argument.
@@ -390,6 +392,7 @@ func ParseCommandLineArgs(argTokens []grub.Token) ([]grubConfigLinuxArg, error) 
 
 		arg := grubConfigLinuxArg{
 			Token:                argToken,
+			Arg:                  argString,
 			Name:                 name,
 			Value:                value,
 			ValueHasVarExpansion: hasVarExpansion,

toolkit/tools/pkg/imagecustomizerlib/partitionutils.go
@@ -433,28 +433,16 @@ func extractKernelCmdlineFromUki(espPartition *diskutils.PartitionInfo,
 	}
 	defer espPartitionMount.Close()
 
-	espLinuxPath := filepath.Join(tmpDirEsp, UkiOutputDir)
-	ukiFiles, err := filepath.Glob(filepath.Join(espLinuxPath, "vmlinuz-*.efi"))
+	cmdlineContents, err := extractKernelCmdlineFromUkiEfis(tmpDirEsp, buildDir)
 	if err != nil {
-		return nil, fmt.Errorf("failed to search for UKI images in ESP partition:\n%w", err)
+		return nil, err
 	}
 
-	if len(ukiFiles) == 0 {
+	if len(cmdlineContents) == 0 {
 		return nil, os.ErrNotExist
 	}
 
-	cmdlinePath := filepath.Join(buildDir, "cmdline.txt")
-	_, _, err = shell.Execute("objcopy", "--dump-section", ".cmdline="+cmdlinePath, ukiFiles[0])
-	if err != nil {
-		return nil, fmt.Errorf("failed to dump kernel cmdline args from UKI:\n%w", err)
-	}
-
-	cmdlineContent, err := os.ReadFile(cmdlinePath)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read kernel cmdline args from dumped file:\n%w", err)
-	}
-
-	tokens, err := grub.TokenizeConfig(string(cmdlineContent))
+	tokens, err := grub.TokenizeConfig(string(cmdlineContents[0]))
 	if err != nil {
 		return nil, fmt.Errorf("failed to tokenize kernel command-line from UKI: %w", err)
 	}
@@ -470,6 +458,33 @@ func extractKernelCmdlineFromUki(espPartition *diskutils.PartitionInfo,
 	}
 
 	return args, nil
+}
+
+func extractKernelCmdlineFromUkiEfis(espPath string, buildDir string) ([]string, error) {
+	cmdlinePath := filepath.Join(buildDir, "cmdline.txt")
+
+	espLinuxPath := filepath.Join(espPath, UkiOutputDir)
+	ukiFiles, err := filepath.Glob(filepath.Join(espLinuxPath, "vmlinuz-*.efi"))
+	if err != nil {
+		return nil, fmt.Errorf("failed to search for UKI images in ESP partition:\n%w", err)
+	}
+
+	cmdlineContents := []string(nil)
+	for _, ukiFile := range ukiFiles {
+		_, _, err = shell.Execute("objcopy", "--dump-section", ".cmdline="+cmdlinePath, ukiFile)
+		if err != nil {
+			return nil, fmt.Errorf("failed to dump kernel cmdline args from UKI (%s):\n%w", ukiFile, err)
+		}
+
+		cmdlineContent, err := os.ReadFile(cmdlinePath)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read kernel cmdline args from dumped file (%s):\n%w", ukiFile, err)
+		}
+
+		cmdlineContents = append(cmdlineContents, string(cmdlineContent))
+	}
+
+	return cmdlineContents, nil
 }
 
 func extractKernelCmdlineFromGrub(bootPartition *diskutils.PartitionInfo,

toolkit/tools/pkg/imagecustomizerlib/customizeuki_test.go
@@ -1,0 +1,71 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+package imagecustomizerlib
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/sys/unix"
+)
+
+func TestCustomizeImageVerityUsrUki(t *testing.T) {
+	imageType := baseImageTypeCoreEfi
+	imageVersion := baseImageVersionAzl3
+	baseImage := checkSkipForCustomizeImage(t, imageType, imageVersion)
+
+	testTempDir := filepath.Join(tmpDir, "TestCustomizeImageUsrVerityUki")
+	buildDir := filepath.Join(testTempDir, "build")
+	outImageFilePath := filepath.Join(testTempDir, "image.raw")
+	configFile := filepath.Join(testDir, "verity-usr-uki.yaml")
+
+	// Customize image.
+	err := CustomizeImageWithConfigFile(buildDir, configFile, baseImage, nil, outImageFilePath, "raw", "",
+		"" /*outputPXEArtifactsDir*/, true /*useBaseImageRpmRepos*/, false /*enableShrinkFilesystems*/)
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	// Connect to customized image.
+	mountPoints := []mountPoint{
+		{
+			PartitionNum:   5,
+			Path:           "/",
+			FileSystemType: "ext4",
+		},
+		{
+			PartitionNum:   2,
+			Path:           "/boot",
+			FileSystemType: "ext4",
+		},
+		{
+			PartitionNum:   1,
+			Path:           "/boot/efi",
+			FileSystemType: "vfat",
+		},
+		{
+			PartitionNum:   3,
+			Path:           "/usr",
+			FileSystemType: "ext4",
+			Flags:          unix.MS_RDONLY,
+		},
+	}
+
+	imageConnection, err := connectToImage(buildDir, outImageFilePath, false /*includeDefaultMounts*/, mountPoints)
+	if !assert.NoError(t, err) {
+		return
+	}
+	defer imageConnection.Close()
+
+	partitions, err := getDiskPartitionsMap(imageConnection.Loopback().DevicePath())
+	assert.NoError(t, err, "get disk partitions")
+
+	// Verify that verity is configured correctly.
+	espPath := filepath.Join(imageConnection.chroot.RootDir(), "/boot/efi")
+	usrDevice := partitionDevPath(imageConnection, 3)
+	usrHashDevice := partitionDevPath(imageConnection, 4)
+	verifyVerityUki(t, espPath, usrDevice, usrHashDevice, "PARTUUID="+partitions[3].PartUuid,
+		"PARTUUID="+partitions[4].PartUuid, "usr", buildDir, "rd.info")
+}

toolkit/tools/pkg/imagecustomizerlib/customizeverity_test.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"path/filepath"
 	"regexp"
+	"strings"
 	"testing"
 
 	"github.com/microsoft/azurelinux/toolkit/tools/imagecustomizerapi"
@@ -82,8 +83,8 @@ func testCustomizeImageVerityHelper(t *testing.T, testName string, imageType bas
 	bootPath := filepath.Join(imageConnection.chroot.RootDir(), "/boot")
 	rootDevice := partitionDevPath(imageConnection, 3)
 	hashDevice := partitionDevPath(imageConnection, 4)
-	verifyVerity(t, bootPath, rootDevice, hashDevice, "PARTUUID="+partitions[3].PartUuid,
-		"PARTUUID="+partitions[4].PartUuid, "root")
+	verifyVerityGrub(t, bootPath, rootDevice, hashDevice, "PARTUUID="+partitions[3].PartUuid,
+		"PARTUUID="+partitions[4].PartUuid, "root", "rd.info", imageVersion)
 }
 
 func TestCustomizeImageVerityShrinkExtract(t *testing.T) {
@@ -157,54 +158,118 @@ func testCustomizeImageVerityShrinkExtractHelper(t *testing.T, testName string, 
 	defer bootMount.Close()
 
 	// Verify that verity is configured correctly.
-	verifyVerity(t, bootMountPath, rootDevice.DevicePath(), hashDevice.DevicePath(), "PARTLABEL=root",
-		"PARTLABEL=roothash", "root")
+	verifyVerityGrub(t, bootMountPath, rootDevice.DevicePath(), hashDevice.DevicePath(), "PARTLABEL=root",
+		"PARTLABEL=roothash", "root", "rd.info", imageVersion)
 }
 
-func verifyVerity(t *testing.T, bootPath string, dataDevice string,
-	hashDevice string, dataId string, hashId string, verityType string,
+func verifyVerityGrub(t *testing.T, bootPath string, dataDevice string, hashDevice string, dataId string, hashId string,
+	verityType string, extraCommandLine string, imageVersion baseImageVersion,
 ) {
-	var hashRegexp *regexp.Regexp
-
-	// Verify verity kernel args.
+	// Extract kernel command line args.
 	grubCfgPath := filepath.Join(bootPath, "/grub2/grub.cfg")
 	grubCfgContents, err := file.Read(grubCfgPath)
 	if !assert.NoError(t, err) {
 		return
 	}
 
-	switch verityType {
-	case "root":
-		assert.Regexp(t, `(?m)linux.* rd.systemd.verity=1 `, grubCfgContents)
-		assert.Regexp(t, fmt.Sprintf(`(?m)linux.* systemd.verity_root_data=%s `, dataId), grubCfgContents)
-		assert.Regexp(t, fmt.Sprintf(`(?m)linux.* systemd.verity_root_hash=%s `, hashId), grubCfgContents)
-		assert.Regexp(t, `(?m)linux.* systemd.verity_root_options=panic-on-corruption `, grubCfgContents)
+	argsRegexp := regexp.MustCompile(`(?m)^[\t ]*linux[\t ]*\S+[\t ]*(.*)$`)
+	matches := argsRegexp.FindAllStringSubmatch(grubCfgContents, -1)
 
-		hashRegexp, err = regexp.Compile(`(?m)linux.* roothash=([a-fA-F0-9]*) `)
-	case "usr":
-		assert.Regexp(t, `(?m)linux.* rd.systemd.verity=1 `, grubCfgContents)
-		assert.Regexp(t, fmt.Sprintf(`(?m)linux.* systemd.verity_usr_data=%s `, dataId), grubCfgContents)
-		assert.Regexp(t, fmt.Sprintf(`(?m)linux.* systemd.verity_usr_hash=%s `, hashId), grubCfgContents)
-		assert.Regexp(t, `(?m)linux.* systemd.verity_usr_options=panic-on-corruption `, grubCfgContents)
-
-		hashRegexp, err = regexp.Compile(`(?m)linux.* usrhash=([a-fA-F0-9]*) `)
-	default:
-		t.Fatalf("Invalid verity type: (%s)", verityType)
+	kernelArgsList := []string(nil)
+	for _, match := range matches {
+		kernelArgsList = append(kernelArgsList, match[1])
 	}
+
+	// Verify verity.
+	verifyVerityHelper(t, kernelArgsList, dataDevice, hashDevice, dataId, hashId, verityType)
+
+	// Verity extra command line args.
+	recoveryCount := 0
+	if imageVersion == baseImageVersionAzl3 {
+		// Count the number of recovery menu items there are.
+		// These menu items won't contain the extra command line args.
+		recoveryCount = strings.Count(grubCfgContents, "(recovery mode)")
+	}
+
+	cmdlineRegexp := regexp.MustCompile(fmt.Sprintf(` %s( |$)`, regexp.QuoteMeta(extraCommandLine)))
+
+	// Count the number of linux lines contain the extra command line args.
+	extracCommandLineMatchCount := 0
+	for _, kernelArgs := range kernelArgsList {
+		if cmdlineRegexp.MatchString(kernelArgs) {
+			extracCommandLineMatchCount += 1
+		}
+	}
+
+	assert.Equal(t, len(kernelArgsList)-recoveryCount, extracCommandLineMatchCount)
+}
+
+func verifyVerityUki(t *testing.T, espPath string, dataDevice string,
+	hashDevice string, dataId string, hashId string, verityType string, buildDir string, extraCommandLine string,
+) {
+	// Extract kernel command line args.
+	kernelArgsList, err := extractKernelCmdlineFromUkiEfis(espPath, buildDir)
 	if !assert.NoError(t, err) {
 		return
 	}
 
-	hashMatches := hashRegexp.FindStringSubmatch(grubCfgContents)
-	if !assert.Equal(t, 2, len(hashMatches)) {
-		return
+	// Verify verity
+	verifyVerityHelper(t, kernelArgsList, dataDevice, hashDevice, dataId, hashId, verityType)
+
+	// Verify extra command line
+	for _, kernelArgs := range kernelArgsList {
+		assert.Regexp(t, fmt.Sprintf(` %s( |$)`, regexp.QuoteMeta(extraCommandLine)), kernelArgs)
+	}
+}
+
+func verifyVerityHelper(t *testing.T, kernelArgsList []string, dataDevice string,
+	hashDevice string, dataId string, hashId string, verityType string,
+) {
+	assert.GreaterOrEqual(t, len(kernelArgsList), 1)
+
+	hash := ""
+	for _, kernelArgs := range kernelArgsList {
+		var hashRegexp *regexp.Regexp
+		switch verityType {
+		case "root":
+			assert.Regexp(t, ` rd.systemd.verity=1 `, kernelArgs)
+			assert.Regexp(t, fmt.Sprintf(` systemd.verity_root_data=%s `, dataId), kernelArgs)
+			assert.Regexp(t, fmt.Sprintf(` systemd.verity_root_hash=%s `, hashId), kernelArgs)
+			assert.Regexp(t, ` systemd.verity_root_options=panic-on-corruption( |$)`, kernelArgs)
+
+			hashRegexp = regexp.MustCompile(` roothash=([a-fA-F0-9]*) `)
+
+		case "usr":
+			assert.Regexp(t, ` rd.systemd.verity=1 `, kernelArgs)
+			assert.Regexp(t, fmt.Sprintf(` systemd.verity_usr_data=%s `, dataId), kernelArgs)
+			assert.Regexp(t, fmt.Sprintf(` systemd.verity_usr_hash=%s `, hashId), kernelArgs)
+			assert.Regexp(t, ` systemd.verity_usr_options=panic-on-corruption( |$)`, kernelArgs)
+
+			hashRegexp = regexp.MustCompile(` usrhash=([a-fA-F0-9]*) `)
+
+		default:
+			t.Errorf("Invalid verity type: (%s)", verityType)
+		}
+
+		hashMatches := hashRegexp.FindStringSubmatch(kernelArgs)
+		if !assert.Equal(t, 2, len(hashMatches)) {
+			continue
+		}
+
+		kernelArgsHash := hashMatches[1]
+		if hash == "" {
+			hash = kernelArgsHash
+		} else {
+			// Ensure all the hashes are the same for all kernel versions.
+			assert.Equal(t, hash, kernelArgsHash)
+		}
 	}
 
-	hash := hashMatches[1]
-
-	// Verify verity hashes.
-	err = shell.ExecuteLive(false, "veritysetup", "verify", dataDevice, hashDevice, hash)
-	assert.NoError(t, err)
+	if assert.NotEqual(t, "", hash) {
+		// Verify verity hashes.
+		err := shell.ExecuteLive(false, "veritysetup", "verify", dataDevice, hashDevice, hash)
+		assert.NoError(t, err)
+	}
 }
 
 func TestCustomizeImageVerityUsr(t *testing.T) {
@@ -271,6 +336,6 @@ func testCustomizeImageVerityUsrHelper(t *testing.T, testName string, imageType 
 	bootPath := filepath.Join(imageConnection.chroot.RootDir(), "/boot")
 	usrDevice := partitionDevPath(imageConnection, 3)
 	hashDevice := partitionDevPath(imageConnection, 4)
-	verifyVerity(t, bootPath, usrDevice, hashDevice, "PARTUUID="+partitions[3].PartUuid,
-		"PARTUUID="+partitions[4].PartUuid, "usr")
+	verifyVerityGrub(t, bootPath, usrDevice, hashDevice, "PARTUUID="+partitions[3].PartUuid,
+		"PARTUUID="+partitions[4].PartUuid, "usr", "rd.info", imageVersion)
 }

toolkit/tools/pkg/imagecustomizerlib/testdata/verity-usr-uki.yaml
@@ -1,0 +1,78 @@
+previewFeatures:
+- uki
+
+storage:
+  bootType: efi
+  disks:
+  - partitionTableType: gpt
+    partitions:
+    - id: esp
+      type: esp
+      size: 250M
+
+    - id: boot
+      size: 250M
+
+    - id: usr
+      size: 1G
+
+    - id: usrhash
+      size: 100M
+
+    - id: root
+      size: 2G
+
+  verity:
+  - id: verityusr
+    name: usr
+    dataDeviceId: usr
+    hashDeviceId: usrhash
+    corruptionOption: panic
+
+  filesystems:
+  - deviceId: esp
+    type: fat32
+    mountPoint:
+      path: /boot/efi
+      options: umask=0077
+
+  - deviceId: boot
+    type: ext4
+    mountPoint: /boot
+
+  - deviceId: root
+    type: ext4
+    mountPoint: /
+
+  - deviceId: verityusr
+    type: ext4
+    mountPoint:
+      path: /usr
+      options: ro
+
+os:
+  bootloader:
+    resetType: hard-reset
+
+  kernelCommandLine:
+    extraCommandLine:
+    - rd.info
+
+  uki:
+    kernels: auto
+
+  users:
+  - name: root
+    password:
+      type: plain-text
+      value: hello
+
+  packages:
+    remove:
+    - grub2-efi-binary
+
+    install:
+    - veritysetup
+    - systemd-ukify
+    - systemd-boot
+    - efibootmgr