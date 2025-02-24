microsoft/azure-linux-image-tools

commit db361b264064751a266907eb028f84636bc052dc

Add multi-kernel support. (#109)
Remove the arbitary restrictions to allow the kernel args to be updated
when multiple kernels are installed.

In addition, remove the logic that sets `"GRUB_DISABLE_RECOVERY"` when
verity is enabled, since the logic can now handle multiple boot entries
in the grub.cfg file when a single kernel is installed.

Also, remove the requirement for `$kernelopts` to exist in the grub.cfg
file, since this logic just complicates the code.

toolkit/tools/pkg/imagecustomizerlib/bootcustomizer.go
@@ -65,7 +65,7 @@ func (b *BootCustomizer) AddKernelCommandLine(extraCommandLine []string) error {
 		b.defaultGrubFileContent = defaultGrubFileContent
 	} else {
 		// Add the args directly to the /boot/grub2/grub.cfg file.
-		grubCfgContent, err := appendKernelCommandLineArgs(b.grubCfgContent, combinedArgs)
+		grubCfgContent, err := appendKernelCommandLineArgsAll(b.grubCfgContent, combinedArgs)
 		if err != nil {
 			return err
 		}
@@ -88,7 +88,7 @@ func (b *BootCustomizer) getSELinuxModeFromGrub() (imagecustomizerapi.SELinuxMod
 			return "", err
 		}
 	} else {
-		args, _, err = getLinuxCommandLineArgs(b.grubCfgContent, true /*requireKernelOpts*/)
+		args, _, err = getLinuxCommandLineArgs(b.grubCfgContent)
 		if err != nil {
 			return imagecustomizerapi.SELinuxModeDefault, err
 		}
@@ -163,7 +163,7 @@ func (b *BootCustomizer) UpdateKernelCommandLineArgs(defaultGrubFileVarName defa
 
 		b.defaultGrubFileContent = defaultGrubFileContent
 	} else {
-		grubCfgContent, err := updateKernelCommandLineArgs(b.grubCfgContent, argsToRemove, newArgs)
+		grubCfgContent, err := updateKernelCommandLineArgsAll(b.grubCfgContent, argsToRemove, newArgs)
 		if err != nil {
 			return err
 		}
@@ -184,34 +184,10 @@ func (b *BootCustomizer) PrepareForVerity() error {
 			return err
 		}
 
-		// Disable recovery menu entry, to avoid having more than 1 linux command in the grub.cfg file.
-		// This will make it easier to modify the grub.cfg file to add the verity args.
-		defaultGrubFileContent, err = UpdateDefaultGrubFileVariable(defaultGrubFileContent, "GRUB_DISABLE_RECOVERY",
-			"true")
-		if err != nil {
-			return err
-		}
-
 		// For rootfs verity, the root device will always be "/dev/mapper/root"
 		rootDevicePath := verityDevicePathFromName(imagecustomizerapi.VerityRootDeviceName)
 		defaultGrubFileContent, err = UpdateDefaultGrubFileVariable(defaultGrubFileContent, "GRUB_DEVICE",
 			rootDevicePath)
-		if err != nil {
-			return err
-		}
-
-		b.defaultGrubFileContent = defaultGrubFileContent
-	}
-
-	return nil
-}
-
-func (b *BootCustomizer) PrepareForUsrVerity() error {
-	if b.isGrubMkconfig {
-		// Disable recovery menu entry, to avoid having more than 1 linux command in the grub.cfg file.
-		// This will make it easier to modify the grub.cfg file to add the verity args.
-		defaultGrubFileContent, err := UpdateDefaultGrubFileVariable(b.defaultGrubFileContent, "GRUB_DISABLE_RECOVERY",
-			"true")
 		if err != nil {
 			return err
 		}

toolkit/tools/pkg/imagecustomizerlib/customizeverity.go
@@ -92,10 +92,6 @@ func prepareGrubConfigForVerity(verityList []imagecustomizerapi.Verity, imageChr
 			if err := bootCustomizer.PrepareForVerity(); err != nil {
 				return err
 			}
-		} else if mountPath == "/usr" {
-			if err := bootCustomizer.PrepareForUsrVerity(); err != nil {
-				return err
-			}
 		}
 	}
 
@@ -126,7 +122,7 @@ func updateGrubConfigForVerity(verityMetadata map[string]verityDeviceMetadata, g
 	// So, instead we just modify the /boot/grub2/grub.cfg file directly.
 	grubMkconfigEnabled := isGrubMkconfigConfig(grub2Config)
 
-	grub2Config, err = updateKernelCommandLineArgs(grub2Config, []string{
+	grub2Config, err = updateKernelCommandLineArgsAll(grub2Config, []string{
 		"rd.systemd.verity", "roothash", "systemd.verity_root_data",
 		"systemd.verity_root_hash", "systemd.verity_root_options",
 		"usrhash", "systemd.verity_usr_data", "systemd.verity_usr_hash",
@@ -140,7 +136,7 @@ func updateGrubConfigForVerity(verityMetadata map[string]verityDeviceMetadata, g
 		rootDevicePath := verityDevicePathFromName(imagecustomizerapi.VerityRootDeviceName)
 
 		if grubMkconfigEnabled {
-			grub2Config, err = updateKernelCommandLineArgs(grub2Config, []string{"root"},
+			grub2Config, err = updateKernelCommandLineArgsAll(grub2Config, []string{"root"},
 				[]string{"root=" + rootDevicePath})
 			if err != nil {
 				return fmt.Errorf("failed to set verity root command-line arg:\n%w", err)

toolkit/tools/pkg/imagecustomizerlib/partitionutils.go
@@ -488,7 +488,7 @@ func extractKernelCmdlineFromGrub(bootPartition *diskutils.PartitionInfo,
 		return nil, fmt.Errorf("failed to read grub.cfg:\n%w", err)
 	}
 
-	args, _, err := getLinuxCommandLineArgs(string(grubCfgContent), true)
+	args, _, err := getLinuxCommandLineArgs(string(grubCfgContent))
 	if err != nil {
 		return nil, fmt.Errorf("failed to extract kernel command-line arguments: %w", err)
 	}

toolkit/tools/pkg/imagecustomizerlib/bootcustomizer_test.go
@@ -30,7 +30,7 @@ func TestBootCustomizerAddKernelCommandLine20(t *testing.T) {
 	expectedGrubCfdDiff := `22c22
 < 	linux $bootprefix/$mariner_linux       rd.auto=1 root=$rootdevice $mariner_cmdline lockdown=integrity sysctl.kernel.unprivileged_bpf_disabled=1 $systemd_cmdline   $kernelopts
 ---
-> 	linux $bootprefix/$mariner_linux       rd.auto=1 root=$rootdevice $mariner_cmdline lockdown=integrity sysctl.kernel.unprivileged_bpf_disabled=1 $systemd_cmdline   console=tty0 console=ttyS0 $kernelopts
+> 	linux $bootprefix/$mariner_linux       rd.auto=1 root=$rootdevice $mariner_cmdline lockdown=integrity sysctl.kernel.unprivileged_bpf_disabled=1 $systemd_cmdline    console=tty0 console=ttyS0 $kernelopts
 `
 	checkDiffs20(t, b, expectedGrubCfdDiff, "")
 }
@@ -113,7 +113,7 @@ func TestBootCustomizerSELinuxMode30(t *testing.T) {
 	expectedDefaultGrubFileDiff := `5c5
 < GRUB_CMDLINE_LINUX="      rd.auto=1 net.ifnames=0 lockdown=integrity "
 ---
-> GRUB_CMDLINE_LINUX="      rd.auto=1 net.ifnames=0 lockdown=integrity  security=selinux selinux=1 "
+> GRUB_CMDLINE_LINUX="      rd.auto=1 net.ifnames=0 lockdown=integrity security=selinux selinux=1  "
 `
 	checkDiffs30(t, b, "", expectedDefaultGrubFileDiff)
 
@@ -127,7 +127,7 @@ func TestBootCustomizerSELinuxMode30(t *testing.T) {
 	expectedDefaultGrubFileDiff = `5c5
 < GRUB_CMDLINE_LINUX="      rd.auto=1 net.ifnames=0 lockdown=integrity "
 ---
-> GRUB_CMDLINE_LINUX="      rd.auto=1 net.ifnames=0 lockdown=integrity   security=selinux selinux=1 enforcing=1 "
+> GRUB_CMDLINE_LINUX="      rd.auto=1 net.ifnames=0 lockdown=integrity  security=selinux selinux=1 enforcing=1  "
 `
 	checkDiffs30(t, b, "", expectedDefaultGrubFileDiff)
 
@@ -141,7 +141,7 @@ func TestBootCustomizerSELinuxMode30(t *testing.T) {
 	expectedDefaultGrubFileDiff = `5c5
 < GRUB_CMDLINE_LINUX="      rd.auto=1 net.ifnames=0 lockdown=integrity "
 ---
-> GRUB_CMDLINE_LINUX="      rd.auto=1 net.ifnames=0 lockdown=integrity     selinux=0 "
+> GRUB_CMDLINE_LINUX="      rd.auto=1 net.ifnames=0 lockdown=integrity    selinux=0  "
 `
 	checkDiffs30(t, b, "", expectedDefaultGrubFileDiff)
 }
@@ -161,9 +161,8 @@ func TestBootCustomizerVerity30(t *testing.T) {
 	err := b.PrepareForVerity()
 	assert.NoError(t, err)
 
-	expectedDefaultGrubFileDiff := `6a7,9
+	expectedDefaultGrubFileDiff := `6a7,8
 > GRUB_DISABLE_UUID="true"
-> GRUB_DISABLE_RECOVERY="true"
 > GRUB_DEVICE="/dev/mapper/root"
 `
 	checkDiffs30(t, b, "", expectedDefaultGrubFileDiff)

toolkit/tools/pkg/imagecustomizerlib/customizebootloader_test.go
@@ -1,0 +1,72 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+package imagecustomizerlib
+
+import (
+	"path/filepath"
+	"regexp"
+	"testing"
+
+	"github.com/microsoft/azurelinux/toolkit/tools/internal/file"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCustomizeImageMultiKernel(t *testing.T) {
+	for _, version := range supportedAzureLinuxVersions {
+		t.Run(string(version), func(t *testing.T) {
+			testCustomizeImageMultiKernel(t, "TestCustomizeImageMultiKernel"+string(version),
+				baseImageTypeCoreEfi, version)
+		})
+	}
+}
+
+func testCustomizeImageMultiKernel(t *testing.T, testName string, imageType baseImageType,
+	imageVersion baseImageVersion,
+) {
+	baseImage := checkSkipForCustomizeImage(t, imageType, imageVersion)
+
+	testTmpDir := filepath.Join(tmpDir, testName)
+	buildDir := filepath.Join(testTmpDir, "build")
+	outImageFilePath := filepath.Join(testTmpDir, "image.raw")
+
+	configFile := ""
+	switch imageVersion {
+	case baseImageVersionAzl2:
+		configFile = filepath.Join(testDir, "multikernel-azl2.yaml")
+
+	case baseImageVersionAzl3:
+		configFile = filepath.Join(testDir, "multikernel-azl3.yaml")
+	}
+
+	// Customize image.
+	err := CustomizeImageWithConfigFile(buildDir, configFile, baseImage, nil, outImageFilePath, "raw", "",
+		"" /*outputPXEArtifactsDir*/, true /*useBaseImageRpmRepos*/, false /*enableShrinkFilesystems*/)
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	imageConnection, err := connectToCoreEfiImage(buildDir, outImageFilePath)
+	if !assert.NoError(t, err) {
+		return
+	}
+	defer imageConnection.Close()
+
+	// Check that the extraCommandLine was added to the grub.cfg file.
+	grubCfgFilePath := filepath.Join(imageConnection.Chroot().RootDir(), "/boot/grub2/grub.cfg")
+	grubCfgContents, err := file.Read(grubCfgFilePath)
+	assert.NoError(t, err, "read grub.cfg file")
+
+	linuxCommandRegex := regexp.MustCompile(`linux.* console=tty0 console=ttyS0 `)
+	matches := linuxCommandRegex.FindAllString(grubCfgContents, -1)
+
+	switch imageVersion {
+	case baseImageVersionAzl2:
+		// AZL2's default grub.cfg file doesn't support multiple kernels.
+		assert.GreaterOrEqual(t, len(matches), 1, "grub.cfg:\n%s", grubCfgContents)
+
+	case baseImageVersionAzl3:
+		// There should be multiple matching linux kernels, one for each installed kernel.
+		assert.GreaterOrEqual(t, len(matches), 2, "grub.cfg:\n%s", grubCfgContents)
+	}
+}

toolkit/tools/pkg/imagecustomizerlib/defaultgrubutils.go
@@ -160,18 +160,10 @@ func GetDefaultGrubFileLinuxArgs(defaultGrubFileContent string, varName defaultG
 		return defaultGrubFileVarAssign{}, nil, 0, err
 	}
 
-	var insertAt int
-	if varName == defaultGrubFileVarNameCmdlineLinuxDefault {
-		// GRUB_CMDLINE_LINUX_DEFAULT variable has the $kernelopts arg.
-		// Any args inserted should be inserted before $kernelopts.
-		insertAt, err = findCommandLineInsertAt(grubTokens, true /*requireKernelOpts*/)
-		if err != nil {
-			err = fmt.Errorf("failed to parse %s's value args:\n%w", varName, err)
-			return defaultGrubFileVarAssign{}, nil, 0, err
-		}
-	} else {
-		// Insert args at the end of the string.
-		insertAt = len(argsString)
+	insertAt, err := findCommandLineInsertAt(grubTokens, len(argsString))
+	if err != nil {
+		err = fmt.Errorf("failed to parse %s's value args:\n%w", varName, err)
+		return defaultGrubFileVarAssign{}, nil, 0, err
 	}
 
 	args, err := ParseCommandLineArgs(grubTokens)

commit a767d03786982afc0ad05859d92d8ad03ce9ca87

Fix recustomizing AZL3 image with verity enabled.
When trying to recustomize an Azure Linux 3.0 image that has verity
enabled, a "more than one 'linux' command in grub config" error occurs.
This problem was caused by #109, which removed `GRUB_DISABLE_RECOVERY`
from the `/etc/default/grub` file, which results in more than 1 menu
item in the `grub.cfg` file.

This change also adds a regression test for this bug.

toolkit/tools/imagecustomizerapi/verity.go
@@ -13,6 +13,9 @@ const (
 
 	VerityRootDeviceName = "root"
 	VerityUsrDeviceName  = "usr"
+
+	VerityRootDevicePath = DeviceMapperPath + "/" + VerityRootDeviceName
+	VerityUsrDevicePath  = DeviceMapperPath + "/" + VerityUsrDeviceName
 )
 
 var (

toolkit/tools/pkg/imagecustomizerlib/bootcustomizer.go
@@ -185,7 +185,7 @@ func (b *BootCustomizer) PrepareForVerity() error {
 		}
 
 		// For rootfs verity, the root device will always be "/dev/mapper/root"
-		rootDevicePath := verityDevicePathFromName(imagecustomizerapi.VerityRootDeviceName)
+		rootDevicePath := imagecustomizerapi.VerityRootDevicePath
 		defaultGrubFileContent, err = UpdateDefaultGrubFileVariable(defaultGrubFileContent, "GRUB_DEVICE",
 			rootDevicePath)
 		if err != nil {

toolkit/tools/pkg/imagecustomizerlib/customizeuki.go
@@ -354,31 +354,13 @@ func createUki(uki *imagecustomizerapi.Uki, buildDir string, buildImageFile stri
 // Note: This function will be optimized by leveraging the internal functions
 // under grubcfgutils.go when implementing bootloader customization.
 func extractKernelToArgsFromGrub(grubCfgPath string) (map[string]string, error) {
-	grubCfgContent, err := file.Read(grubCfgPath)
+	kernelToArgs, err := extracKernelCmdlineFromGrubFile(grubCfgPath)
 	if err != nil {
-		return nil, fmt.Errorf("failed to read grub.cfg file at (%s):\n%w", grubCfgPath, err)
+		return nil, err
 	}
 
-	lines, err := FindNonRecoveryLinuxLine(grubCfgContent)
-	if err != nil {
-		return nil, fmt.Errorf("failed to find linux command lines in grub.cfg:\n%w", err)
-	}
-
-	kernelToArgs := make(map[string]string)
-	for _, line := range lines {
-		if len(line.Tokens) < 3 {
-			return nil, fmt.Errorf("linux line in grub.cfg file has less than 3 args")
-		}
-
-		kernel := line.Tokens[1].RawContent
-		kernel = strings.TrimPrefix(kernel, "/")
-
-		argTokens := line.Tokens[2:]
-		args, err := ParseCommandLineArgs(argTokens)
-		if err != nil {
-			return nil, fmt.Errorf("failed to parse linux command lines args (%s):\n%w", kernel, err)
-		}
-
+	kernelToArgsString := make(map[string]string)
+	for kernel, args := range kernelToArgs {
 		filteredArgs := []string(nil)
 		for _, arg := range args {
 			if arg.ValueHasVarExpansion {
@@ -390,10 +372,10 @@ func extractKernelToArgsFromGrub(grubCfgPath string) (map[string]string, error) 
 		}
 
 		filteredArgsString := GrubArgsToString(filteredArgs)
-		kernelToArgs[kernel] = filteredArgsString
+		kernelToArgsString[kernel] = filteredArgsString
 	}
 
-	return kernelToArgs, nil
+	return kernelToArgsString, nil
 }
 
 func buildUki(kernel string, initramfs string, cmdlineFilePath string, osSubreleaseFullPath string,

toolkit/tools/pkg/imagecustomizerlib/customizeverity.go
@@ -133,7 +133,7 @@ func updateGrubConfigForVerity(verityMetadata map[string]verityDeviceMetadata, g
 	}
 
 	if _, exists := verityMetadata["/"]; exists {
-		rootDevicePath := verityDevicePathFromName(imagecustomizerapi.VerityRootDeviceName)
+		rootDevicePath := imagecustomizerapi.VerityRootDevicePath
 
 		if grubMkconfigEnabled {
 			grub2Config, err = updateKernelCommandLineArgsAll(grub2Config, []string{"root"},

toolkit/tools/pkg/imagecustomizerlib/partitionutils.go
@@ -307,22 +307,14 @@ func findExtendedPartition(mountIdType ExtendedMountIdentifierType, mountId stri
 	partitions []diskutils.PartitionInfo, buildDir string,
 ) (diskutils.PartitionInfo, int, error) {
 	if mountIdType == ExtendedMountIdentifierTypeDev {
-		cmdline, err := extractKernelCmdline(partitions, buildDir)
+		newMountIdType, newMountId, err := findDevPathPartition(mountId, partitions, buildDir)
 		if err != nil {
+			err = fmt.Errorf("failed to find partition (%s):\n%w", mountId, err)
 			return diskutils.PartitionInfo{}, 0, err
 		}
 
-		if mountId == verityDevicePathFromName(imagecustomizerapi.VerityRootDeviceName) {
-			mountIdType, mountId, err = extractVerityPartitionId(cmdline, "systemd.verity_root_data", imagecustomizerapi.VerityRootDeviceName)
-			if err != nil {
-				return diskutils.PartitionInfo{}, 0, err
-			}
-		} else if mountId == verityDevicePathFromName(imagecustomizerapi.VerityUsrDeviceName) {
-			mountIdType, mountId, err = extractVerityPartitionId(cmdline, "systemd.verity_usr_data", imagecustomizerapi.VerityUsrDeviceName)
-			if err != nil {
-				return diskutils.PartitionInfo{}, 0, err
-			}
-		}
+		mountIdType = newMountIdType
+		mountId = newMountId
 	}
 
 	matchedPartitionIndexes := []int(nil)
@@ -354,6 +346,28 @@ func findExtendedPartition(mountIdType ExtendedMountIdentifierType, mountId stri
 	partition := partitions[partitionIndex]
 
 	return partition, partitionIndex, nil
+}
+
+func findDevPathPartition(mountId string, partitions []diskutils.PartitionInfo,
+	buildDir string,
+) (ExtendedMountIdentifierType, string, error) {
+	cmdline, err := extractKernelCmdline(partitions, buildDir)
+	if err != nil {
+		return "", "", err
+	}
+
+	switch mountId {
+	case imagecustomizerapi.VerityRootDevicePath:
+		return extractVerityPartitionId(cmdline, "systemd.verity_root_data",
+			imagecustomizerapi.VerityRootDeviceName)
+
+	case imagecustomizerapi.VerityUsrDevicePath:
+		return extractVerityPartitionId(cmdline, "systemd.verity_usr_data",
+			imagecustomizerapi.VerityUsrDeviceName)
+
+	default:
+		return "", "", fmt.Errorf("unknown partition id type (%s)", mountId)
+	}
 }
 
 func extractKernelCmdline(partitions []diskutils.PartitionInfo, buildDir string) ([]grubConfigLinuxArg, error) {
@@ -457,14 +471,9 @@ func extractKernelCmdlineFromGrub(bootPartition *diskutils.PartitionInfo,
 	defer bootPartitionMount.Close()
 
 	grubCfgPath := filepath.Join(tmpDirBoot, DefaultGrubCfgPath)
-	grubCfgContent, err := os.ReadFile(grubCfgPath)
+	kernelToArgs, err := extracKernelCmdlineFromGrubFile(grubCfgPath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read grub.cfg:\n%w", err)
-	}
-
-	args, _, err := getLinuxCommandLineArgs(string(grubCfgContent))
-	if err != nil {
-		return nil, fmt.Errorf("failed to extract kernel command-line arguments: %w", err)
 	}
 
 	err = bootPartitionMount.CleanClose()
@@ -472,7 +481,47 @@ func extractKernelCmdlineFromGrub(bootPartition *diskutils.PartitionInfo,
 		return nil, fmt.Errorf("failed to close bootPartitionMount:\n%w", err)
 	}
 
-	return args, nil
+	for _, args := range kernelToArgs {
+		// Pick the first set of the args.
+		// (Hopefully they are all the same.)
+		return args, nil
+	}
+
+	return nil, fmt.Errorf("no kernel args found in grub.cfg file")
+}
+
+// Extracts the kernel args for each kernel from the grub.cfg file.
+// Returns a mapping from kernel version to list of kernel args.
+func extracKernelCmdlineFromGrubFile(grubCfgPath string) (map[string][]grubConfigLinuxArg, error) {
+	grubCfgContent, err := file.Read(grubCfgPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read grub.cfg file at (%s):\n%w", grubCfgPath, err)
+	}
+
+	lines, err := FindNonRecoveryLinuxLine(grubCfgContent)
+	if err != nil {
+		return nil, fmt.Errorf("failed to find linux command lines in grub.cfg:\n%w", err)
+	}
+
+	kernelToArgs := make(map[string][]grubConfigLinuxArg)
+	for _, line := range lines {
+		if len(line.Tokens) < 3 {
+			return nil, fmt.Errorf("linux line in grub.cfg file has less than 3 args")
+		}
+
+		kernel := line.Tokens[1].RawContent
+		kernel = strings.TrimPrefix(kernel, "/")
+
+		argTokens := line.Tokens[2:]
+		args, err := ParseCommandLineArgs(argTokens)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse linux command lines args (%s):\n%w", kernel, err)
+		}
+
+		kernelToArgs[kernel] = args
+	}
+
+	return kernelToArgs, nil
 }
 
 func extractVerityPartitionId(cmdline []grubConfigLinuxArg, verityDataArg string,

toolkit/tools/pkg/imagecustomizerlib/customizeverity_test.go
@@ -45,6 +45,21 @@ func testCustomizeImageVerityHelper(t *testing.T, testName string, imageType bas
 		return
 	}
 
+	verityRootVerity(t, imageType, imageVersion, buildDir, outImageFilePath)
+
+	// Recustomize the image.
+	err = CustomizeImageWithConfigFile(buildDir, configFile, outImageFilePath, nil, outImageFilePath, "raw", "",
+		"" /*outputPXEArtifactsDir*/, true /*useBaseImageRpmRepos*/, false /*enableShrinkFilesystems*/)
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	verityRootVerity(t, imageType, imageVersion, buildDir, outImageFilePath)
+}
+
+func verityRootVerity(t *testing.T, imageType baseImageType, imageVersion baseImageVersion, buildDir string,
+	outImageFilePath string,
+) {
 	// Connect to customized image.
 	mountPoints := []mountPoint{
 		{
@@ -80,11 +95,17 @@ func testCustomizeImageVerityHelper(t *testing.T, testName string, imageType bas
 	assert.NoError(t, err, "get disk partitions")
 
 	// Verify that verity is configured correctly.
+	// This helps verify that verity-enabled images can be recustomized.
 	bootPath := filepath.Join(imageConnection.chroot.RootDir(), "/boot")
 	rootDevice := partitionDevPath(imageConnection, 3)
 	hashDevice := partitionDevPath(imageConnection, 4)
 	verifyVerityGrub(t, bootPath, rootDevice, hashDevice, "PARTUUID="+partitions[3].PartUuid,
 		"PARTUUID="+partitions[4].PartUuid, "root", "rd.info", imageVersion)
+
+	err = imageConnection.CleanClose()
+	if !assert.NoError(t, err) {
+		return
+	}
 }
 
 func TestCustomizeImageVerityShrinkExtract(t *testing.T) {
@@ -294,6 +315,22 @@ func testCustomizeImageVerityUsrHelper(t *testing.T, testName string, imageType 
 		return
 	}
 
+	verityUsrVerity(t, imageType, imageVersion, buildDir, outImageFilePath)
+
+	// Recustomize image.
+	// This helps verify that verity-enabled images can be recustomized.
+	err = CustomizeImageWithConfigFile(buildDir, configFile, outImageFilePath, nil, outImageFilePath, "raw", "",
+		"" /*outputPXEArtifactsDir*/, true /*useBaseImageRpmRepos*/, false /*enableShrinkFilesystems*/)
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	verityUsrVerity(t, imageType, imageVersion, buildDir, outImageFilePath)
+}
+
+func verityUsrVerity(t *testing.T, imageType baseImageType, imageVersion baseImageVersion, buildDir string,
+	outImageFilePath string,
+) {
 	// Connect to usr verity image.
 	mountPoints := []mountPoint{
 		{