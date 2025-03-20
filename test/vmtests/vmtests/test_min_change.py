209d9ab9815c1d9f2b114fb9929a193c2e8ae3c5
George Mileka <gmileka@outlook.com>
2025-03-19 19:11:37 -0700

    Enable azl3 efi qcow

--- test/vmtests/vmtests/test_min_change.py
+++ test/vmtests/vmtests/test_min_change.py
@@ -222,41 +222,10 @@ def run_min_change_test(
                 assert False, "Unexpected image identity in /etc/os-release"
 
 
-# def test_min_change_efi_azl2_qcow_output(
-#     docker_client: DockerClient,
-#     image_customizer_container_url: str,
-#     core_efi_azl2: Path,
-#     ssh_key: Tuple[str, Path],
-#     test_temp_dir: Path,
-#     test_instance_name: str,
-#     output_images_dir: Path,
-#     libvirt_conn: libvirt.virConnect,
-#     close_list: List[Closeable],
-# ) -> None:
-#     azl_release = 2
-#     config_path = TEST_CONFIGS_DIR.joinpath("os-vm-config.yaml")
-#     output_format = "qcow2"
-
-#     run_min_change_test(
-#         docker_client,
-#         image_customizer_container_url,
-#         core_efi_azl2,
-#         azl_release,
-#         config_path,
-#         output_format,
-#         ssh_key,
-#         test_temp_dir,
-#         test_instance_name,
-#         output_images_dir,
-#         libvirt_conn,
-#         close_list,
-#     )
-
-
-def test_min_change_efi_azl3_qcow_output(
+def test_min_change_efi_azl2_qcow_output(
     docker_client: DockerClient,
     image_customizer_container_url: str,
-    core_efi_azl3: Path,
+    core_efi_azl2: Path,
     ssh_key: Tuple[str, Path],
     test_temp_dir: Path,
     test_instance_name: str,
@@ -264,14 +233,14 @@ def test_min_change_efi_azl3_qcow_output(
     libvirt_conn: libvirt.virConnect,
     close_list: List[Closeable],
 ) -> None:
-    azl_release = 3
+    azl_release = 2
     config_path = TEST_CONFIGS_DIR.joinpath("os-vm-config.yaml")
     output_format = "qcow2"
 
     run_min_change_test(
         docker_client,
         image_customizer_container_url,
-        core_efi_azl3,
+        core_efi_azl2,
         azl_release,
         config_path,
         output_format,
@@ -282,6 +251,37 @@ def test_min_change_efi_azl3_qcow_output(
         libvirt_conn,
         close_list,
     )
+
+# ok
+# def test_min_change_efi_azl3_qcow_output(
+#     docker_client: DockerClient,
+#     image_customizer_container_url: str,
+#     core_efi_azl3: Path,
+#     ssh_key: Tuple[str, Path],
+#     test_temp_dir: Path,
+#     test_instance_name: str,
+#     output_images_dir: Path,
+#     libvirt_conn: libvirt.virConnect,
+#     close_list: List[Closeable],
+# ) -> None:
+#     azl_release = 3
+#     config_path = TEST_CONFIGS_DIR.joinpath("os-vm-config.yaml")
+#     output_format = "qcow2"
+
+#     run_min_change_test(
+#         docker_client,
+#         image_customizer_container_url,
+#         core_efi_azl3,
+#         azl_release,
+#         config_path,
+#         output_format,
+#         ssh_key,
+#         test_temp_dir,
+#         test_instance_name,
+#         output_images_dir,
+#         libvirt_conn,
+#         close_list,
+#     )
 
 
 # def test_min_change_legacy_azl2_qcow_output(
