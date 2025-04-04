868182bba5961ef1b1aeb23bdbbde7f0044c7cfa
George Mileka <gmileka@outlook.com>
2025-04-03 19:41:39 -0700

    Keep only one test for now

--- test/vmtests/vmtests/test_min_change.py
+++ test/vmtests/vmtests/test_min_change.py
@@ -187,38 +187,38 @@ def test_min_change_efi_azl2_qcow_output(
     )
 
 
-def test_min_change_efi_azl3_qcow_output(
-    docker_client: DockerClient,
-    image_customizer_container_url: str,
-    core_efi_azl3: Path,
-    ssh_key: Tuple[str, Path],
-    test_temp_dir: Path,
-    test_instance_name: str,
-    output_artifacts_dir: Path,
-    libvirt_conn: libvirt.virConnect,
-    close_list: List[Closeable],
-) -> None:
-    azl_release = 3
-    if platform.machine() == 'x86_64':
-        config_path = TEST_CONFIGS_DIR.joinpath("os-vm-config.yaml")
-    else:
-        config_path = TEST_CONFIGS_DIR.joinpath("os-vm-config-arm64.yaml")
-    output_format = "qcow2"
+# def test_min_change_efi_azl3_qcow_output(
+#     docker_client: DockerClient,
+#     image_customizer_container_url: str,
+#     core_efi_azl3: Path,
+#     ssh_key: Tuple[str, Path],
+#     test_temp_dir: Path,
+#     test_instance_name: str,
+#     output_artifacts_dir: Path,
+#     libvirt_conn: libvirt.virConnect,
+#     close_list: List[Closeable],
+# ) -> None:
+#     azl_release = 3
+#     if platform.machine() == 'x86_64':
+#         config_path = TEST_CONFIGS_DIR.joinpath("os-vm-config.yaml")
+#     else:
+#         config_path = TEST_CONFIGS_DIR.joinpath("os-vm-config-arm64.yaml")
+#     output_format = "qcow2"
 
-    run_min_change_test(
-        docker_client,
-        image_customizer_container_url,
-        core_efi_azl3,
-        azl_release,
-        config_path,
-        output_format,
-        ssh_key,
-        test_temp_dir,
-        test_instance_name,
-        output_artifacts_dir,
-        libvirt_conn,
-        close_list,
-    )
+#     run_min_change_test(
+#         docker_client,
+#         image_customizer_container_url,
+#         core_efi_azl3,
+#         azl_release,
+#         config_path,
+#         output_format,
+#         ssh_key,
+#         test_temp_dir,
+#         test_instance_name,
+#         output_artifacts_dir,
+#         libvirt_conn,
+#         close_list,
+#     )
 
 @pytest.mark.skipif(platform.machine() != 'x86_64', reason="arm64 is not supported for this combination")
 def test_min_change_legacy_azl2_qcow_output(
