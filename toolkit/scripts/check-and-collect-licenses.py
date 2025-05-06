16ab1ff1cbe6f890f5e2becfc590d365cc4e81af
Roaa Sakr <romoh@microsoft.com>
2025-05-06 10:53:56 -0700

    Move trivy installation to script (#227)
    Makes it easier to call makefile without any prereqs
    
    ---------
    
    Signed-off-by: Roaa Sakr <romoh@microsoft.com>

--- toolkit/scripts/check-and-collect-licenses.py
+++ toolkit/scripts/check-and-collect-licenses.py
@@ -2,18 +2,74 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
+import hashlib
 import io
 import json
+import os
 from pathlib import Path
+import platform
 import shutil
 import subprocess
 import sys
+import tarfile
+import tempfile
+import urllib
+import urllib.request
 
 REPO_ROOT = subprocess.run(["git", "rev-parse", "--show-toplevel"], capture_output=True, text=True, check=True).stdout.strip()
 OUTPUT_DIR = Path(REPO_ROOT) / "toolkit" / "out"
 LICENSE_SCAN_OUTPUT = OUTPUT_DIR / "LICENSES-SCAN.json"
 LICENSES_DIR = OUTPUT_DIR / "LICENSES"
 TOOLS_DIR = Path(REPO_ROOT) / "toolkit" / "tools"
+
+def download_trivy():
+    TRIVY_VERSION = "0.61.1"
+    TRIVY_FILENAME = f"trivy_{TRIVY_VERSION}_Linux-64bit.tar.gz"
+    TRIVY_URL = f"https://github.com/aquasecurity/trivy/releases/download/v{TRIVY_VERSION}/{TRIVY_FILENAME}"
+
+    print("Downloading Trivy...")
+
+    if shutil.which("trivy"):
+        print("Trivy is already installed. Skipping installation.")
+        return
+
+    machine = platform.machine()
+    arch = "64bit"
+    expected_sha256 = "dcc6f48c383833f3a8ee0380f7990a17c89e36553cdf34e1f2d3159f9d8270ec"
+    if machine == "aarch64":
+        arch = "ARM64"
+        expected_sha256 = "a2f28808626ae08877279e13a32d9cc8f845bdfdc762a692b2f32768326208a6"
+
+    TRIVY_FILENAME = f"trivy_{TRIVY_VERSION}_Linux-{arch}.tar.gz"
+    TRIVY_URL = f"https://github.com/aquasecurity/trivy/releases/download/v{TRIVY_VERSION}/{TRIVY_FILENAME}"
+    BIN_PATH = "/usr/local/bin/trivy"
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        tar_path = os.path.join(tmpdir, TRIVY_FILENAME)
+
+        try:
+            urllib.request.urlretrieve(TRIVY_URL, tar_path)
+        except Exception as e:
+            print(f"Download Trivy failed: {e}")
+            sys.exit(1)
+
+        sha256 = hashlib.sha256()
+        with open(tar_path, "rb") as f:
+            for chunk in iter(lambda: f.read(4096), b""):
+                sha256.update(chunk)
+
+        print("Verifying checksum...")
+        if sha256.hexdigest() != expected_sha256:
+            print("SHA256 checksum does not match!")
+            sys.exit(1)
+
+        with tarfile.open(tar_path, "r:gz") as tar:
+            tar.extractall(path=tmpdir)
+
+        subprocess.run(["sudo", "mv", os.path.join(tmpdir, "trivy"), BIN_PATH], check=True)
+        os.remove(tar_path)
+
+    print("Trivy installed successfully.")
 
 def run_trivy_scan():
     print("Running Trivy license scan...")
@@ -104,5 +160,6 @@ def collect_licenses():
     print(f"✅ License files copied to {LICENSES_DIR}.")
 
 if __name__ == "__main__":
+    download_trivy()
     run_trivy_scan()
     collect_licenses()
