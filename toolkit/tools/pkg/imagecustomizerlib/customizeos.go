5dba4f0be1ae9103a0b4259d6c867a726d45a67f
George Mileka <gmileka@outlook.com>
2025-05-16 11:36:20 -0700

    customize.go clean-up

--- toolkit/tools/pkg/imagecustomizerlib/customizeos.go
+++ toolkit/tools/pkg/imagecustomizerlib/customizeos.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"github.com/microsoft/azurelinux/toolkit/tools/imagecustomizerapi"
-	"github.com/microsoft/azurelinux/toolkit/tools/internal/logger"
 )
 
 func doOsCustomizations(buildDir string, baseConfigPath string, config *imagecustomizerapi.Config,
@@ -24,22 +23,16 @@ func doOsCustomizations(buildDir string, baseConfigPath string, config *imagecus
 		return err
 	}
 
-	logger.Log.Infof("OS - Installing packages")
-
 	err = addRemoveAndUpdatePackages(buildDir, baseConfigPath, config.OS, imageChroot, rpmsSources,
 		useBaseImageRpmRepos)
 	if err != nil {
 		return err
 	}
 
-	logger.Log.Infof("OS - Updating hostname")
-
 	err = UpdateHostname(config.OS.Hostname, imageChroot)
 	if err != nil {
 		return err
 	}
-
-	logger.Log.Infof("OS - Copying additional files and dirs")
 
 	err = copyAdditionalDirs(baseConfigPath, config.OS.AdditionalDirs, imageChroot)
 	if err != nil {
@@ -51,28 +44,20 @@ func doOsCustomizations(buildDir string, baseConfigPath string, config *imagecus
 		return err
 	}
 
-	logger.Log.Infof("OS - Updating users")
-
 	err = AddOrUpdateUsers(config.OS.Users, baseConfigPath, imageChroot)
 	if err != nil {
 		return err
 	}
-
-	logger.Log.Infof("OS - Enabling/Disabling services")
 
 	err = EnableOrDisableServices(config.OS.Services, imageChroot)
 	if err != nil {
 		return err
 	}
 
-	logger.Log.Infof("OS - Enabling/Disabling modules")
-
 	err = LoadOrDisableModules(config.OS.Modules, imageChroot.RootDir())
 	if err != nil {
 		return err
 	}
-
-	logger.Log.Infof("OS - Customizing release")
 
 	err = addCustomizerRelease(imageChroot.RootDir(), ToolVersion, buildTime, imageUuid)
 	if err != nil {
@@ -80,23 +65,16 @@ func doOsCustomizations(buildDir string, baseConfigPath string, config *imagecus
 	}
 
 	if config.OS.ImageHistory != imagecustomizerapi.ImageHistoryNone {
-
-		logger.Log.Infof("OS - Adding image history")
-
 		err = addImageHistory(imageChroot.RootDir(), imageUuid, baseConfigPath, ToolVersion, buildTime, config)
 		if err != nil {
 			return err
 		}
 	}
 
-	logger.Log.Infof("OS - Handle bootloader")
-
 	err = handleBootLoader(baseConfigPath, config, imageConnection)
 	if err != nil {
 		return err
 	}
-
-	logger.Log.Infof("OS - Handle selinux")
 
 	selinuxMode, err := handleSELinux(config.OS.SELinux.Mode, config.OS.BootLoader.ResetType,
 		imageChroot)
@@ -104,14 +82,10 @@ func doOsCustomizations(buildDir string, baseConfigPath string, config *imagecus
 		return err
 	}
 
-	logger.Log.Infof("OS - Enable overlays")
-
 	overlayUpdated, err := enableOverlays(config.OS.Overlays, selinuxMode, imageChroot)
 	if err != nil {
 		return err
 	}
-
-	logger.Log.Infof("OS - Enable verity partition")
 
 	verityUpdated, err := enableVerityPartition(config.Storage.Verity, imageChroot)
 	if err != nil {
@@ -119,50 +93,36 @@ func doOsCustomizations(buildDir string, baseConfigPath string, config *imagecus
 	}
 
 	if partitionsCustomized || overlayUpdated || verityUpdated {
-		logger.Log.Infof("OS - Regenerate initd")
-
 		err = regenerateInitrd(imageChroot)
 		if err != nil {
 			return err
 		}
 	}
 
-	logger.Log.Infof("OS - Run user postCustomization")
-
 	err = runUserScripts(baseConfigPath, config.Scripts.PostCustomization, "postCustomization", imageChroot)
 	if err != nil {
 		return err
 	}
-
-	logger.Log.Infof("OS - Prepare uki")
 
 	err = prepareUki(buildDir, config.OS.Uki, imageChroot)
 	if err != nil {
 		return err
 	}
 
-	logger.Log.Infof("OS - Restore resolve conf")
-
 	err = restoreResolvConf(resolvConf, imageChroot)
 	if err != nil {
 		return err
 	}
-
-	logger.Log.Infof("OS - Set selinux files")
 
 	err = selinuxSetFiles(selinuxMode, imageChroot)
 	if err != nil {
 		return err
 	}
 
-	logger.Log.Infof("OS - Run user finalizeCustomization")
-
 	err = runUserScripts(baseConfigPath, config.Scripts.FinalizeCustomization, "finalizeCustomization", imageChroot)
 	if err != nil {
 		return err
 	}
-
-	logger.Log.Infof("OS - Check installed kernel")
 
 	err = checkForInstalledKernel(imageChroot)
 	if err != nil {
