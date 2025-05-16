40ad693de00b013fcd2260533a6da565e9b643af
George Mileka <gmileka@outlook.com>
2025-05-16 13:52:04 -0700

    initrdutils.go clean-up - part 2

--- toolkit/tools/internal/initrdutils/initrdutils.go
+++ toolkit/tools/internal/initrdutils/initrdutils.go
@@ -13,7 +13,6 @@ import (
 	"github.com/cavaliercoder/go-cpio"
 	"github.com/klauspost/pgzip"
 
-	"github.com/microsoft/azurelinux/toolkit/tools/internal/logger"
 	"github.com/microsoft/azurelinux/toolkit/tools/internal/shell"
 )
 
@@ -49,7 +48,7 @@ func CreateInitrdImageFromFolder(inputDir, outputInitrdImagePath string) (err er
 		if fileErr != nil {
 			return fmt.Errorf("encountered a file walk error on path (%s):\n%w", path, fileErr)
 		}
-		err = addFileToArchive(inputDir, path, info, cpioWriter)
+		err = addFileToCpioArchive(inputDir, path, info, cpioWriter)
 		if err != nil {
 			return fmt.Errorf("failed to add (%s) to archive:\n%w", path, err)
 		}
@@ -66,14 +65,6 @@ func buildCpioHeader(inputDir, path string, info os.FileInfo, link string) (cpio
 		return nil, fmt.Errorf("failed to convert OS file info into a cpio header for (%s)\n%w", path, err)
 	}
 
-	// Get owners (cpio.FileInfoHeader() does not fill out this part)
-	stat, ok := info.Sys().(*syscall.Stat_t)
-	if !ok {
-		return nil, fmt.Errorf("failed to get file stat of (%s)", path)
-	}
-	cpioHeader.UID = int(stat.Uid)
-	cpioHeader.GID = int(stat.Gid)
-
 	// Convert full path to relative path
 	relPath, err := filepath.Rel(inputDir, path)
 	if err != nil {
@@ -81,11 +72,18 @@ func buildCpioHeader(inputDir, path string, info os.FileInfo, link string) (cpio
 	}
 	cpioHeader.Name = relPath
 
+	// Set owners (cpio.FileInfoHeader() does not set the owners)
+	stat, ok := info.Sys().(*syscall.Stat_t)
+	if !ok {
+		return nil, fmt.Errorf("failed to get file stat of (%s)", path)
+	}
+	cpioHeader.UID = int(stat.Uid)
+	cpioHeader.GID = int(stat.Gid)
+
 	return cpioHeader, nil
 }
 
-func addFileToArchive(inputDir, path string, info os.FileInfo, cpioWriter *cpio.Writer) (err error) {
-	// Symlinks need to be resolved to their target file to be added to the cpio archive.
+func addFileToCpioArchive(inputDir, path string, info os.FileInfo, cpioWriter *cpio.Writer) (err error) {
 	var link string
 	if info.Mode()&os.ModeSymlink != 0 {
 		link, err = os.Readlink(path)
@@ -116,8 +114,6 @@ func addFileToArchive(inputDir, path string, info os.FileInfo, cpioWriter *cpio.
 			return fmt.Errorf("failed to write (%s) to cpio archive\n%w", path, err)
 		}
 	} else {
-		// For a symlink, the reported size will be the size (in bytes) of the link's target.
-		// Write this data into the archive.
 		if info.Mode()&os.ModeSymlink != 0 {
 			_, err = cpioWriter.Write([]byte(link))
 			if err != nil {
@@ -125,41 +121,65 @@ func addFileToArchive(inputDir, path string, info os.FileInfo, cpioWriter *cpio.
 			}
 		}
 
-		// For all other special files, they will be of size 0 and only contain the header in the archive.
+		// For all other special files, they will be of size 0 and only contain
+		// the header in the archive.
 	}
 
 	return nil
 }
 
-func CreateFolderFromInitrdImage(inputInitrdImagePath, outputDir string) error {
+func updateFileOwnership(path string, fileMode os.FileMode, uid, gid int) (err error) {
+	err = os.Chown(path, uid, gid)
+	if err != nil {
+		return fmt.Errorf("failed to set ownership on extracted (%s) to (%d,%d):\n%w", path, uid, gid, err)
+	}
+
+	if fileMode&cpio.ModePerm != 0 {
+		// os.Chmod() does not set the higher-order bits (setuid, setgid, and
+		// setsticky). So, we are calling the `chmod` command-line instead.
+		fileModeString := fmt.Sprintf("%#o", fileMode)
+		chmodParams := []string{
+			fileModeString,
+			path,
+		}
+		err = shell.ExecuteLive(true /*squashErrors*/, "chmod", chmodParams...)
+		if err != nil {
+			return fmt.Errorf("failed to change mode of file (%s) to (%s): %w", fileModeString, path, err)
+		}
+	}
+	return nil
+}
+
+func CreateFolderFromInitrdImage(inputInitrdImagePath, outputDir string) (err error) {
+	// Open the input archive
 	inputInitrdImageFile, err := os.Open(inputInitrdImagePath)
 	if err != nil {
-		return fmt.Errorf("open archive: %w", err)
+		return fmt.Errorf("failed to open archive(%s):\n%w", inputInitrdImagePath, err)
 	}
 	defer inputInitrdImageFile.Close()
 
 	// Create pgzip reader
-	gzr, err := pgzip.NewReader(inputInitrdImageFile)
+	pgzipReader, err := pgzip.NewReader(inputInitrdImageFile)
 	if err != nil {
 		return fmt.Errorf("create pgzip reader: %w", err)
 	}
-	defer gzr.Close()
+	defer pgzipReader.Close()
 
 	// Create cpio reader
-	cpioReader := cpio.NewReader(gzr)
+	cpioReader := cpio.NewReader(pgzipReader)
 
 	for {
-		hdr, err := cpioReader.Next()
+		cpioHeader, err := cpioReader.Next()
 		if err == io.EOF {
 			break // end of archive
 		}
 		if err != nil {
-			return fmt.Errorf("read cpio header: %w", err)
+			return fmt.Errorf("failed to read cpio header from (%s):\n%w", inputInitrdImagePath, err)
 		}
 
-		path := filepath.Join(outputDir, hdr.Name)
-		fileMode := os.FileMode(hdr.Mode & (cpio.ModePerm | cpio.ModeSetuid | cpio.ModeSetgid | cpio.ModeSticky))
-		fileType := hdr.Mode & cpio.ModeType
+		path := filepath.Join(outputDir, cpioHeader.Name)
+		fileMode := os.FileMode(cpioHeader.Mode & (cpio.ModePerm | cpio.ModeSetuid | cpio.ModeSetgid | cpio.ModeSticky))
+		fileType := cpioHeader.Mode & cpio.ModeType
 
 		switch fileType {
 		case cpio.ModeDir:
@@ -168,29 +188,9 @@ func CreateFolderFromInitrdImage(inputInitrdImagePath, outputDir string) error {
 				return fmt.Errorf("failed to create directory %s: %w", path, err)
 			}
 
-			if hdr.UID != 0 || hdr.GID != 0 {
-				logger.Log.Infof("---- unpacking ---- %d, %d, %s", hdr.UID, hdr.GID, path)
-				// time.Sleep(20 * time.Second)
-			}
-
-			err = os.Chown(path, hdr.UID, hdr.GID)
+			err = updateFileOwnership(path, fileMode, cpioHeader.UID, cpioHeader.GID)
 			if err != nil {
-				return fmt.Errorf("failed to set ownership on extracted file (%s) to (%d,%d):\n%w", path, hdr.UID, hdr.GID, err)
-			}
-
-			if fileMode > 0777 {
-				// For some reason, Chmod does not set the higher-order bits:
-				// setuid, setgid, and setsticky bits
-				// err = os.Chmod(path, fileMode)
-				fileModeString := fmt.Sprintf("%#o", fileMode)
-				chmodParams := []string{
-					fileModeString,
-					path,
-				}
-				err = shell.ExecuteLive(true /*squashErrors*/, "chmod", chmodParams...)
-				if err != nil {
-					return fmt.Errorf("failed to change mode of file %s: %w", path, err)
-				}
+				return fmt.Errorf("failed to update ownership of (%s)\n%w", path, err)
 			}
 
 		case cpio.ModeRegular:
@@ -204,52 +204,13 @@ func CreateFolderFromInitrdImage(inputInitrdImagePath, outputDir string) error {
 				return fmt.Errorf("write file %s: %w", path, err)
 			}
 
-			err = os.Chown(path, hdr.UID, hdr.GID)
+			err = updateFileOwnership(path, fileMode, cpioHeader.UID, cpioHeader.GID)
 			if err != nil {
-				return fmt.Errorf("failed to set ownership on extracted file (%s) to (%d,%d):\n%w", path, hdr.UID, hdr.GID, err)
-			}
-
-			if fileMode > 0777 {
-				// For some reason, Chmod does not set the higher-order bits:
-				// setuid, setgid, and setsticky bits
-				// err = os.Chmod(path, fileMode)
-				fileModeString := fmt.Sprintf("%#o", fileMode)
-				chmodParams := []string{
-					fileModeString,
-					path,
-				}
-				err = shell.ExecuteLive(true /*squashErrors*/, "chmod", chmodParams...)
-				if err != nil {
-					return fmt.Errorf("failed to change mode of file %s: %w", path, err)
-				}
+				return fmt.Errorf("failed to update ownership of (%s)\n%w", path, err)
 			}
 
 		case cpio.ModeSymlink:
-			pathDir := filepath.Dir(path)
-			_, err := os.Stat(pathDir)
-			if err != nil {
-				if os.IsNotExist(err) {
-					logger.Log.Debugf("                 --> Directory (%s) does not exists!", pathDir)
-					// ToDo: why 755?
-					err := os.MkdirAll(pathDir, 0755)
-					if err != nil {
-						return fmt.Errorf("failed to create directory %s: %w", path, err)
-					}
-				} else {
-					return fmt.Errorf("failed to check directory %s: %w", pathDir, err)
-				}
-			}
-
-			os.Symlink(hdr.Linkname, path)
-		case cpio.ModeDevice:
-			logger.Log.Debugf("-- [dev     ] [%#o] (%s)", fileMode, path)
-			return fmt.Errorf("unsupported file type 'Device' in CPIO archive.")
-		case cpio.ModeCharDevice:
-			logger.Log.Debugf("-- [char dev] [%#o] (%s)", fileMode, path)
-			return fmt.Errorf("unsupported file type 'Char Device' in CPIO archive.")
-		case cpio.ModeSocket:
-			logger.Log.Debugf("-- [socket  ] [%#o] (%s)", fileMode, path)
-			return fmt.Errorf("unsupported file type 'Socket' in CPIO archive.")
+			os.Symlink(cpioHeader.Linkname, path)
 		default:
 			return fmt.Errorf("unsupported unknown type %#o in CPIO archive.", fileType)
 		}
