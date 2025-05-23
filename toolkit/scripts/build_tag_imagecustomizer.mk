f876285c63f4047911a565f54aeae69378d12bb7
Chris Gunn <cwize1@gmail.com>
2025-05-23 11:29:43 -0700

    Patch release GitHub action.
    Create a GitHub action workflow for building and publishing patch
    releases. These are mostly the same as major/minor releases but they
    don't fork a release branch and they don't use Makefile for the version.
    Instead patch releases auto-generate the next release version based on
    the previous git tags.

--- toolkit/scripts/build_tag_imagecustomizer.mk
+++ toolkit/scripts/build_tag_imagecustomizer.mk
@@ -4,12 +4,12 @@
 # Azure Linux Image Customizer version.
 # This is using semantic versioning.
 #
-# image_customizer_version should have the format:
+# IMAGE_CUSTOMIZER_VERSION should have the format:
 #
 #   <major>.<minor>.<patch>
 #
 # and should hold the value of the next (or current) official release, not the previous official
 # release.
-image_customizer_version ?= 0.15.0
+IMAGE_CUSTOMIZER_VERSION ?= 0.14.0
 IMAGE_CUSTOMIZER_VERSION_PREVIEW ?= -dev.$(DATETIME_AS_VERSION)+$(GIT_COMMIT_ID)
-image_customizer_full_version := $(image_customizer_version)$(IMAGE_CUSTOMIZER_VERSION_PREVIEW)
+image_customizer_full_version := $(IMAGE_CUSTOMIZER_VERSION)$(IMAGE_CUSTOMIZER_VERSION_PREVIEW)
