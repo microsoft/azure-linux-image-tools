2f4940f5470c5297e8faf5ff96074dee47f46365
George Mileka <gmileka@outlook.com>
2025-05-30 12:13:11 -0700

    MIC: ISO/PXE: full os initramfs support - Part 1 - Image support

--- toolkit/tools/pkg/imagecustomizerlib/liveosisoimages.go
+++ toolkit/tools/pkg/imagecustomizerlib/liveosisoimages.go
@@ -4,7 +4,10 @@
 package imagecustomizerlib
 
 import (
+	"archive/tar"
+	"compress/gzip"
 	"fmt"
+	"io"
 	"io/fs"
 	"os"
 	"path/filepath"
@@ -441,5 +444,125 @@ func createWriteableImageFromArtifacts(buildDir string, filesStore *IsoFilesStor
 		return err
 	}
 
+	return nil
+}
+
+func createTarGzArchive(sourceDir, outputArchivePath string) error {
+	logger.Log.Infof("Creating archive (%s) from (%s)", outputArchivePath, sourceDir)
+
+	outFile, err := os.Create(outputArchivePath)
+	if err != nil {
+		return fmt.Errorf("failed to create archive (%s):\n%w", outputArchivePath, err)
+	}
+	defer outFile.Close()
+
+	gw := gzip.NewWriter(outFile)
+	defer gw.Close()
+
+	tw := tar.NewWriter(gw)
+	defer tw.Close()
+
+	err = filepath.Walk(sourceDir, func(file string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		header, err := tar.FileInfoHeader(info, info.Name())
+		if err != nil {
+			return err
+		}
+
+		// Adjust the header name to maintain folder structure
+		relPath, err := filepath.Rel(sourceDir, file)
+		if err != nil {
+			return err
+		}
+		header.Name = filepath.ToSlash(relPath) // Ensure forward slashes
+
+		if err := tw.WriteHeader(header); err != nil {
+			return err
+		}
+
+		// If it's a directory, nothing more to do
+		if info.IsDir() {
+			return nil
+		}
+
+		// Write file contents
+		f, err := os.Open(file)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		_, err = io.Copy(tw, f)
+		return err
+	})
+
+	if err != nil {
+		return fmt.Errorf("failed to create archive (%s):\n%w", outputArchivePath, err)
+	}
+
+	return nil
+}
+
+func expandTarGzArchive(sourceArchivePath, outputDir string) error {
+	logger.Log.Infof("Expanding archive (%s) to (%s)", sourceArchivePath, outputDir)
+
+	f, err := os.Open(sourceArchivePath)
+	if err != nil {
+		return fmt.Errorf("failed to archive (%s):\n%w", sourceArchivePath, err)
+	}
+	defer f.Close()
+
+	gzr, err := gzip.NewReader(f)
+	if err != nil {
+		return fmt.Errorf("failed to create gzip reader for (%s):\n%w", sourceArchivePath, err)
+	}
+	defer gzr.Close()
+
+	tr := tar.NewReader(gzr)
+	for {
+		header, err := tr.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return fmt.Errorf("failed to read header from archive:\n%w", err)
+		}
+
+		target := filepath.Join(outputDir, header.Name)
+
+		switch header.Typeflag {
+		case tar.TypeDir:
+			if err := os.MkdirAll(target, os.FileMode(header.Mode)); err != nil {
+				return fmt.Errorf("failed to create folder (%s)\n%w", target, err)
+			}
+		case tar.TypeReg:
+			if err := os.MkdirAll(filepath.Dir(target), 0755); err != nil {
+				return fmt.Errorf("failed to create parent folder for (%s)\n%w", target, err)
+			}
+			outFile, err := os.Create(target)
+			if err != nil {
+				return fmt.Errorf("failed to create (%s):\n%w", target, err)
+			}
+			defer outFile.Close()
+			_, err = io.Copy(outFile, tr)
+			if err != nil {
+				return fmt.Errorf("failed to copy (%s) from archive:\n%w", target, err)
+			}
+			outFile.Close()
+
+			if err := os.Chmod(target, os.FileMode(header.Mode)); err != nil {
+				return fmt.Errorf("failed to set permissions (%d) on (%s):\n%w", os.FileMode(header.Mode), target, err)
+			}
+		case tar.TypeSymlink:
+			if err := os.Symlink(header.Linkname, target); err != nil {
+				return fmt.Errorf("failed to create symbolic link (%s):\n%w", target, err)
+			}
+		default:
+			return fmt.Errorf("failed to process unsupported file type in archive (%s): (%v)", target, header.Typeflag)
+		}
+	}
 	return nil
 }
