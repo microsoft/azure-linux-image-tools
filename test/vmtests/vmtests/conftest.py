347eb823e904880c80d34aac777e07f853d8e01c
Himaja Kesari <himaja.reddy96@gmail.com>
2025-07-29 12:49:45 -0700

    add vm tests
    run vm tests for creator
    
    fix make check
    
    fix make check
    
    fix make check
    
    fix download rpms step
    
    add build step
    
    fix the rpms path
    
    fix rpm sources
    
    add path
    
    fix test failures
    
    fix build issues
    
    install prereqs
    
    uncomment
    
    fix bad rebase
    
    restructure

--- test/vmtests/vmtests/conftest.py
+++ test/vmtests/vmtests/conftest.py
@@ -30,9 +30,13 @@ def pytest_addoption(parser: pytest.Parser) -> None:
     parser.addoption("--core-legacy-azl3", action="store", help="Path to input image")
     parser.addoption("--logs-dir", action="store", help="Path to logs directory")
     parser.addoption("--image-customizer-container-url", action="store", help="Image Customizer container image URL")
+    parser.addoption("--image-creator-binary-path", action="store", help="Path to Image Creator binary")
+    parser.addoption("--rpm-sources", action="append", help="Path to RPM source files or directories")
+    parser.addoption("--tools-tar", action="store", help="Path to tools tar file")
     parser.addoption(
         "--ssh-private-key", action="store", help="An SSH private key file to use for authentication with the VMs"
     )
+    parser.addoption("--image-customizer-binary-path", action="store", help="Path to Image Customizer binary")
 
 
 @pytest.fixture(scope="session")
@@ -123,6 +127,38 @@ def image_customizer_container_url(request: pytest.FixtureRequest) -> Generator[
 
 
 @pytest.fixture(scope="session")
+def image_creator_binary_path(request: pytest.FixtureRequest) -> Generator[Path, None, None]:
+    binary_path = request.config.getoption("--image-creator-binary-path")
+    if not binary_path:
+        raise Exception("--image-creator-binary-path is required for imagecreator tests")
+    yield Path(binary_path)
+
+
+@pytest.fixture(scope="session")
+def rpm_sources(request: pytest.FixtureRequest) -> Generator[List[Path], None, None]:
+    sources = request.config.getoption("--rpm-sources")
+    if not sources:
+        raise Exception("--rpm-sources is required for test")
+    yield [Path(source) for source in sources]
+
+
+@pytest.fixture(scope="session")
+def tools_tar(request: pytest.FixtureRequest) -> Generator[Path, None, None]:
+    tar_path = request.config.getoption("--tools-tar")
+    if not tar_path:
+        raise Exception("--tools-tar is required for test")
+    yield Path(tar_path)
+
+
+@pytest.fixture(scope="session")
+def image_customizer_binary_path(request: pytest.FixtureRequest) -> Generator[Path, None, None]:
+    binary_path = request.config.getoption("--image-customizer-binary-path")
+    if not binary_path:
+        raise Exception("--image-customizer-binary-path is required for test")
+    yield Path(binary_path)
+
+
+@pytest.fixture(scope="session")
 def docker_client() -> Generator[DockerClient, None, None]:
     client = docker.from_env()
     yield client
@@ -182,7 +218,7 @@ def close_list(keep_environment: bool) -> Generator[List[Closeable], None, None]
             exceptions.append(ex)
 
     if len(exceptions) > 0:
-        raise ExceptionGroup("failed to close resources", exceptions)
+        return ExceptionGroup("failed to close resources", exceptions)
 
 
 def create_temp_folder(
