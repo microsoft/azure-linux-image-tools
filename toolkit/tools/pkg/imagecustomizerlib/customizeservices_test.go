7a744d10116ee53cc6250e6137ef7bf55c3fbb70
Adit Jha <aditjha@microsoft.com>
2025-07-31 16:05:00 +0000

    Fix test error message assertion

--- toolkit/tools/pkg/imagecustomizerlib/customizeservices_test.go
+++ toolkit/tools/pkg/imagecustomizerlib/customizeservices_test.go
@@ -89,6 +89,6 @@ func TestCustomizeImageServicesDisableUnknown(t *testing.T) {
 
 	err := CustomizeImage(t.Context(), buildDir, testDir, &config, baseImage, nil, outImageFilePath, "raw",
 		true /*useBaseImageRpmRepos*/, "" /*packageSnapshotTime*/)
-	assert.ErrorContains(t, err, "failed to disable service (chocolate-chip-muffin)")
+	assert.ErrorContains(t, err, "failed to disable service (service='chocolate-chip-muffin')")
 	assert.ErrorContains(t, err, "No such file or directory")
 }
