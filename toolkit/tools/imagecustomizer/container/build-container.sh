c143f4dc4c8ec32c38184405083afa59730b5080
Sean Dougherty <sdougherty@microsoft.com>
2025-08-18 17:36:54 +0000

    keep sysemd-ukify as an installed package in arm64

--- toolkit/tools/imagecustomizer/container/build-container.sh
+++ toolkit/tools/imagecustomizer/container/build-container.sh
@@ -97,8 +97,8 @@ touch "${containerStagingFolder}"/.mariner-toolkit-ignore-dockerenv
 
 # azl doesn't support grub2-pc for arm64, hence remove it from dockerfile
 if [ "$ARCH" == "arm64" ]; then
-    echo "Removing grub2-pc and systemd-ukify from Dockerfile for arm64"
-    sed -i 's/\<grub2-pc systemd-ukify\>//g' "$dockerFile"
+    echo "Removing grub2-pc from Dockerfile for arm64"
+    sed -i 's/\<grub2-pc\>//g' "$dockerFile"
 fi
 
 # list all staged files
