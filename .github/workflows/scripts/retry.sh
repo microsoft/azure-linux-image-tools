b499652e8e28aa94daab20c9b5aa63db83f0c5d4
Chris Gunn <chrisgun@microsoft.com>
2025-08-29 12:11:41 -0700

    Add retry.sh script

---
+++ .github/workflows/scripts/retry.sh
@@ -0,0 +1,25 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+# The script runs a command in a retry loop.
+#
+# Usage:
+#
+#   retry.sh RETRY_COUNT SLEEP_TIME COMMAND ARGS...
+
+RETRY_COUNT="$1"
+SLEEP_TIME="$2"
+
+shift 2
+
+for ((i = 1; ; i++)); do
+    "$@" \
+        && break \
+        || err=$?
+
+    if [ "$i" == 5 ]; then
+        exit $err
+    fi
+
+    sleep "$SLEEP_TIME"
+done
