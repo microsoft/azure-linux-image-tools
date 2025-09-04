0ec10df0e4aefa39b6c2eac6122d70507f3ed35d
Chris Gunn <chrisgun@microsoft.com>
2025-09-04 15:51:26 -0700

    For 'inject-files', shrink non-verity partitions for COSI output.
    When using the 'inject-files' subcommand, if the output format is COSI
    then shrink the non-verity partitions. This is to be more consistent
    with the customize subcommand, which shrinks all partitions when
    outputting as COSI.
    
    It is not possible for 'inject-files' to shrink verity partitions since
    that would invalidate the verity hash tree. In the future, we may add
    an API to allow the customize command to shrink verity partitions even
    when not outputting as COSI.

--- toolkit/tools/pkg/imagecustomizerlib/artifactsinputoutput_test.go
+++ toolkit/tools/pkg/imagecustomizerlib/artifactsinputoutput_test.go
@@ -9,8 +9,14 @@ import (
 
 	"github.com/microsoft/azure-linux-image-tools/toolkit/tools/imagecustomizerapi"
 	"github.com/microsoft/azure-linux-image-tools/toolkit/tools/internal/file"
+	"github.com/microsoft/azure-linux-image-tools/toolkit/tools/internal/safeloopback"
+	"github.com/microsoft/azure-linux-image-tools/toolkit/tools/internal/safemount"
 	"github.com/microsoft/azure-linux-image-tools/toolkit/tools/internal/testutils"
 	"github.com/stretchr/testify/assert"
+)
+
+const (
+	pseudoSignedMarker = "##TEST_MARKER_INJECTED##"
 )
 
 func TestOutputAndInjectArtifacts(t *testing.T) {
@@ -47,85 +53,10 @@ func TestOutputAndInjectArtifacts(t *testing.T) {
 		return
 	}
 
-	// Confirm inject-files.yaml was generated
-	injectConfigPath := filepath.Join(outputArtifactsDir, "inject-files.yaml")
-	exists, err := file.PathExists(injectConfigPath)
-	assert.NoError(t, err)
-	assert.True(t, exists, "Expected inject-files.yaml to be generated")
-
-	var injectConfig imagecustomizerapi.InjectFilesConfig
-	err = imagecustomizerapi.UnmarshalYamlFile(injectConfigPath, &injectConfig)
-	assert.NoError(t, err)
-
-	// Check previewFeatures
-	assert.Contains(t, injectConfig.PreviewFeatures, imagecustomizerapi.PreviewFeatureInjectFiles, "Expected previewFeatures to include 'inject-files'")
-
-	// Check artifacts
-	hasShim := false
-	hasSystemdBoot := false
-	hasUKI := false
-
-	for _, entry := range injectConfig.InjectFiles {
-		switch {
-		case strings.HasPrefix(entry.Destination, "/EFI/BOOT/boot") &&
-			strings.HasSuffix(entry.Destination, ".efi") &&
-			strings.HasPrefix(entry.Source, "./boot") &&
-			strings.HasSuffix(entry.Source, ".signed.efi"):
-			hasShim = true
-		case strings.HasPrefix(entry.Destination, "/EFI/systemd/systemd-boot") &&
-			strings.HasSuffix(entry.Destination, ".efi") &&
-			strings.HasPrefix(entry.Source, "./systemd-boot") &&
-			strings.HasSuffix(entry.Source, ".signed.efi"):
-			hasSystemdBoot = true
-		case strings.HasPrefix(entry.Destination, "/EFI/Linux/vmlinuz") &&
-			strings.HasSuffix(entry.Destination, ".efi") &&
-			strings.HasPrefix(entry.Source, "./vmlinuz") &&
-			strings.HasSuffix(entry.Source, ".signed.efi"):
-			hasUKI = true
-		}
-	}
-
-	assert.True(t, hasShim, "Expected an inject entry for shim")
-	assert.True(t, hasSystemdBoot, "Expected an inject entry for systemd-boot")
-	assert.True(t, hasUKI, "Expected at least one inject entry for UKI")
-
-	// Confirm artifacts were outputted
-	// Detect boot binary
-	bootBinaries, err := filepath.Glob(filepath.Join(outputArtifactsDir, "boot*.efi"))
-	assert.NoError(t, err)
-	assert.Equal(t, 1, len(bootBinaries), "Expected exactly one boot<arch>.efi binary")
-	bootBinary := bootBinaries[0]
-
-	// Detect systemd-boot binary
-	systemdBootBinaries, err := filepath.Glob(filepath.Join(outputArtifactsDir, "systemd-boot*.efi"))
-	assert.NoError(t, err)
-	assert.Equal(t, 1, len(systemdBootBinaries), "Expected exactly one systemd-boot<arch>.efi binary")
-	systemdBootBinary := systemdBootBinaries[0]
-
-	// Detect unsigned UKIs
-	ukiFiles, err := filepath.Glob(filepath.Join(outputArtifactsDir, "vmlinuz-*.efi"))
-	assert.NoError(t, err)
-	assert.GreaterOrEqual(t, len(ukiFiles), 1, "Expected at least one UKI file")
-
-	// Simulate signed boot & systemd-boot
-	marker := "##TEST_MARKER_INJECTED##"
-	for _, src := range []string{bootBinary, systemdBootBinary} {
-		dst := replaceSuffix(src, ".efi", ".signed.efi")
-		err := file.Copy(src, dst)
-		assert.NoError(t, err)
-
-		err = appendMarker(dst, marker)
-		assert.NoError(t, err)
-	}
-
-	// Simulate signed UKIs
-	for _, src := range ukiFiles {
-		dst := replaceSuffix(src, ".efi", ".signed.efi")
-		err := file.Copy(src, dst)
-		assert.NoError(t, err)
-	}
+	espFiles := verifyAndSignOutputtedArtifacts(t, outputArtifactsDir)
 
 	// Inject artifacts into a fresh copy of the raw image
+	injectConfigPath := filepath.Join(outputArtifactsDir, "inject-files.yaml")
 	err = InjectFilesWithConfigFile(t.Context(), buildDir, injectConfigPath, outImageFilePath, "", "")
 	if !assert.NoError(t, err) {
 		return
@@ -158,27 +89,169 @@ func TestOutputAndInjectArtifacts(t *testing.T) {
 	}
 	defer imageConnection.Close()
 
-	// Check the injected files
-	// shim
-	expectedInjectedShim := filepath.Join(imageConnection.Chroot().RootDir(), "boot/efi/EFI/BOOT", filepath.Base(bootBinary))
-	contains, err := fileContainsMarker(expectedInjectedShim, marker)
-	assert.NoError(t, err)
-	assert.True(t, contains, "Expected injected shim to exist:\n%s", expectedInjectedShim)
+	verifyInjectedFiles(t, filepath.Join(imageConnection.Chroot().RootDir(), "boot/efi"), espFiles)
+}
 
-	// systemd-boot
-	expectedInjectedSystemdBoot := filepath.Join(imageConnection.Chroot().RootDir(), "boot/efi/EFI/systemd", filepath.Base(systemdBootBinary))
-	contains, err = fileContainsMarker(expectedInjectedSystemdBoot, marker)
-	assert.NoError(t, err)
-	assert.True(t, contains, "Expected injected systemd-boot to exist:\n%s", expectedInjectedSystemdBoot)
-
-	// UKI(s)
-	for _, src := range ukiFiles {
-		expectedInjectedUKI := filepath.Join(imageConnection.Chroot().RootDir(), "boot/efi/EFI/Linux", filepath.Base(src))
-
-		exists, err = file.PathExists(expectedInjectedUKI)
-		assert.NoError(t, err)
-		assert.True(t, exists, "Expected injected UKI to exist:\n%s", expectedInjectedUKI)
+func TestOutputAndInjectArtifactsCosi(t *testing.T) {
+	baseImage, baseImageInfo := checkSkipForCustomizeDefaultImage(t)
+	if baseImageInfo.Version == baseImageVersionAzl2 {
+		t.Skip("'systemd-boot' is not available on Azure Linux 2.0")
 	}
+
+	ukifyExists, err := file.CommandExists("ukify")
+	assert.NoError(t, err)
+	if !ukifyExists {
+		t.Skip("The 'ukify' command is not available")
+	}
+
+	if runtime.GOARCH == "arm64" {
+		t.Skip("systemd-boot not available on AZL3 ARM64 yet")
+	}
+
+	testTempDir := filepath.Join(tmpDir, "TestOutputAndInjectArtifacts")
+	buildDir := filepath.Join(testTempDir, "build")
+	outImageFilePath := filepath.Join(testTempDir, "image.raw")
+	cosiFilePath := filepath.Join(testTempDir, "image.cosi")
+	originalConfigFile := filepath.Join(testDir, "artifacts-output.yaml")
+	configFile := filepath.Join(testTempDir, "artifacts-output.yaml")
+	outputArtifactsDir := filepath.Join(testTempDir, "output")
+	injectConfigPath := filepath.Join(outputArtifactsDir, "inject-files.yaml")
+
+	// Copy test config to the temp dir so it's isolated
+	err = file.Copy(originalConfigFile, configFile)
+	assert.NoError(t, err)
+
+	// Customize image.
+	err = CustomizeImageWithConfigFile(t.Context(), buildDir, configFile, baseImage, nil, outImageFilePath, "raw",
+		true /*useBaseImageRpmRepos*/, "" /*packageSnapshotTime*/)
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	espFiles := verifyAndSignOutputtedArtifacts(t, outputArtifactsDir)
+
+	// Inject artifacts into image.
+	err = InjectFilesWithConfigFile(t.Context(), buildDir, injectConfigPath, outImageFilePath, cosiFilePath, "cosi")
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	// Connect to image.
+	partitionsPaths, err := extractPartitionsFromCosi(cosiFilePath, testTempDir)
+	if !assert.NoError(t, err) || !assert.Len(t, partitionsPaths, 3) {
+		return
+	}
+
+	espPartitionPath := filepath.Join(testTempDir, "image_1.raw")
+	//bootPartitionPath := filepath.Join(testTempDir, "image_2.raw")
+	//rootPartitionPath := filepath.Join(testTempDir, "image_3.raw")
+
+	espDevice, err := safeloopback.NewLoopback(espPartitionPath)
+	if !assert.NoError(t, err) {
+		return
+	}
+	defer espDevice.Close()
+
+	espMountPath := filepath.Join(testTempDir, "esppartition")
+	espMount, err := safemount.NewMount(espDevice.DevicePath(), espMountPath, "vfat", 0, "", true)
+	if !assert.NoError(t, err) {
+		return
+	}
+	defer espMount.Close()
+
+	verifyInjectedFiles(t, espMountPath, espFiles)
+}
+
+func verifyAndSignOutputtedArtifacts(t *testing.T, outputArtifactsDir string) []string {
+	// Confirm inject-files.yaml was generated
+	injectConfigPath := filepath.Join(outputArtifactsDir, "inject-files.yaml")
+	exists, err := file.PathExists(injectConfigPath)
+	assert.NoError(t, err)
+	assert.True(t, exists, "Expected inject-files.yaml to be generated")
+
+	var injectConfig imagecustomizerapi.InjectFilesConfig
+	err = imagecustomizerapi.UnmarshalYamlFile(injectConfigPath, &injectConfig)
+	assert.NoError(t, err)
+
+	// Check previewFeatures
+	assert.Contains(t, injectConfig.PreviewFeatures, imagecustomizerapi.PreviewFeatureInjectFiles, "Expected previewFeatures to include 'inject-files'")
+
+	// Check artifacts
+	hasShim := false
+	hasSystemdBoot := false
+	hasUKI := false
+	espFiles := []string(nil)
+
+	for _, entry := range injectConfig.InjectFiles {
+		switch {
+		case strings.HasPrefix(entry.Destination, "/EFI/BOOT/boot") &&
+			strings.HasSuffix(entry.Destination, ".efi") &&
+			strings.HasPrefix(entry.Source, "./boot") &&
+			strings.HasSuffix(entry.Source, ".signed.efi"):
+			hasShim = true
+
+			espFiles = append(espFiles, entry.Destination)
+
+		case strings.HasPrefix(entry.Destination, "/EFI/systemd/systemd-boot") &&
+			strings.HasSuffix(entry.Destination, ".efi") &&
+			strings.HasPrefix(entry.Source, "./systemd-boot") &&
+			strings.HasSuffix(entry.Source, ".signed.efi"):
+			hasSystemdBoot = true
+
+			espFiles = append(espFiles, entry.Destination)
+
+		case strings.HasPrefix(entry.Destination, "/EFI/Linux/vmlinuz") &&
+			strings.HasSuffix(entry.Destination, ".efi") &&
+			strings.HasPrefix(entry.Source, "./vmlinuz") &&
+			strings.HasSuffix(entry.Source, ".signed.efi"):
+			hasUKI = true
+
+			espFiles = append(espFiles, entry.Destination)
+		}
+
+		// Check that the unsigned file exists.
+		unsignedPath := filepath.Join(outputArtifactsDir, entry.UnsignedSource)
+		_, err := os.Stat(unsignedPath)
+		assert.NoErrorf(t, err, "failed to check if unsigned file exists ('%s')", unsignedPath)
+
+		// Pseudo sign the file.
+		signedPath := filepath.Join(outputArtifactsDir, entry.Source)
+		err = pseudoSignFile(unsignedPath, signedPath)
+		assert.NoErrorf(t, err, "pseduo sign file failed (unsignedPath='%s', signedPath='%s')", unsignedPath, signedPath)
+	}
+
+	// Ensure all the expected files were seen.
+	assert.Equal(t, 3, len(injectConfig.InjectFiles))
+	assert.True(t, hasShim, "Expected an inject entry for shim")
+	assert.True(t, hasSystemdBoot, "Expected an inject entry for systemd-boot")
+	assert.True(t, hasUKI, "Expected at least one inject entry for UKI")
+
+	return espFiles
+}
+
+func verifyInjectedFiles(t *testing.T, partitionDir string, partitionFiles []string) {
+	// Check the injected files.
+	for _, filePath := range partitionFiles {
+		injectedFilePath := filepath.Join(partitionDir, filePath)
+
+		contains, err := fileContainsMarker(injectedFilePath, pseudoSignedMarker)
+		assert.NoError(t, err)
+		assert.Truef(t, contains, "Expected injected file to exist (%s)", injectedFilePath)
+	}
+}
+
+func pseudoSignFile(inputPath string, outputPath string) error {
+	err := file.Copy(inputPath, outputPath)
+	if err != nil {
+		return err
+	}
+
+	err = appendMarker(outputPath, pseudoSignedMarker)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func appendMarker(path string, marker string) error {
