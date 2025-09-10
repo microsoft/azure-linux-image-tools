c566f4739bff3d3959a1d16b9eec72a52285ddb1
Chris Gunn <chrisgun@microsoft.com>
2025-09-10 13:51:48 -0700

    Support forking before publishing new release.
    Add support for forking a release branch before doing a release.
    
    Firstly, update the `Release (major/minor)` workflow with an option to
    not publish the release and only fork the release branch.
    
    Secondly, update the `Release (patch)` workflow's logic that increments
    the patch version so that it supports the case where no release has
    occured for the major/minor version yet.

--- .github/workflows/scripts/next_patch_version.py
+++ .github/workflows/scripts/next_patch_version.py
@@ -7,19 +7,32 @@ from version_utils import getMakefileVersion, getVersionTags
 
 def main():
     makefileVersion = getMakefileVersion()
+    makefileVersionMgrMin = makefileVersion[0:2]
 
     versions = getVersionTags(merged=True)
-    if len(versions) <= 0:
-        print(f"No previous tags found", file=sys.stderr)
-        exit(2)
 
-    mostRecentVersion = versions[0]
+    mostRecentVersion = None
+    mostRecentVersionMgrMin = None
+    if len(versions) > 0:
+        mostRecentVersion = versions[0]
+        mostRecentVersionMgrMin = mostRecentVersion[0:2]
 
-    if mostRecentVersion[0] != makefileVersion[0] or mostRecentVersion[1] != makefileVersion[1]:
-        print(f"Makefile and most recent major/minor versions don't match: {makefileVersion} vs. {mostRecentVersion}", file=sys.stderr)
+    if mostRecentVersion is not None and makefileVersionMgrMin < mostRecentVersionMgrMin:
+        print(f"Makefile major/minor version is less than most recent major/minor version: "+
+            f"{makefileVersion} vs. {mostRecentVersion}",
+            file=sys.stderr)
         exit(3)
 
-    newPatchVersion = (mostRecentVersion[0], mostRecentVersion[1], mostRecentVersion[2]+1)
+    elif mostRecentVersion is not None and makefileVersionMgrMin == mostRecentVersionMgrMin:
+        # A previous version tag for the current major/minor version exists.
+        # So, increment the patch version.
+        newPatchVersion = (mostRecentVersion[0], mostRecentVersion[1], mostRecentVersion[2]+1)
+
+    else:
+        # A version tag doesn't doesn't exist yet for the current major/minor version.
+        # So, start from patch 0.
+        newPatchVersion = makefileVersion
+
     print(f"{newPatchVersion[0]}.{newPatchVersion[1]}.{newPatchVersion[2]}")
 
 if __name__ == "__main__":
