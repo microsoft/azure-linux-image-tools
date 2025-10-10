dc29e1dab2089141fa4ef1c868e268278321bc94
Chris Gunn <cwize1@gmail.com>
2025-10-10 13:03:00 -0700

    WIP: ValidateConfig refactor

--- toolkit/tools/pkg/imagecreatorlib/imagecreator.go
+++ toolkit/tools/pkg/imagecreatorlib/imagecreator.go
@@ -5,13 +5,9 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/microsoft/azure-linux-image-tools/toolkit/tools/imagecustomizerapi"
-	"github.com/microsoft/azure-linux-image-tools/toolkit/tools/imagegen/diskutils"
-	"github.com/microsoft/azure-linux-image-tools/toolkit/tools/internal/file"
 	"github.com/microsoft/azure-linux-image-tools/toolkit/tools/internal/logger"
-	"github.com/microsoft/azure-linux-image-tools/toolkit/tools/internal/randomization"
 	"github.com/microsoft/azure-linux-image-tools/toolkit/tools/internal/safechroot"
 	"github.com/microsoft/azure-linux-image-tools/toolkit/tools/pkg/imagecustomizerlib"
 )
@@ -19,33 +15,6 @@ import (
 const (
 	setupRoot = "/setuproot"
 )
-
-type ImageCreatorParameters struct {
-	// build dirs
-	buildDirAbs string
-
-	// configurations
-	configPath          string
-	config              *imagecustomizerapi.Config
-	rpmsSources         []string
-	packageSnapshotTime string
-	toolsTar            string
-
-	// output image
-	outputImageFormat imagecustomizerapi.ImageFormatType
-	outputImageFile   string
-	outputImageDir    string
-	outputImageBase   string
-
-	// raw image file
-	rawImageFile string
-
-	imageUuid    [randomization.UuidSize]byte
-	imageUuidStr string
-
-	partUuidToFstabEntry map[string]diskutils.FstabEntry
-	osRelease            string
-}
 
 func CreateImageWithConfigFile(ctx context.Context, buildDir string, configFile string, rpmsSources []string,
 	toolsTar string, outputImageFile string, outputImageFormat string, distro string, distroVersion string,
@@ -78,18 +47,11 @@ func createNewImage(ctx context.Context, buildDir string, baseConfigPath string,
 	rpmsSources []string, outputImageFile string, outputImageFormat string, toolsTar string, distro string,
 	distroVersion string, packageSnapshotTime string,
 ) error {
-	err := validateConfig(
+	rc, err := validateConfig(
 		ctx, baseConfigPath, &config, rpmsSources, toolsTar, outputImageFile,
 		outputImageFormat, packageSnapshotTime)
 	if err != nil {
 		return err
-	}
-
-	imageCreatorParameters, err := createImageCreatorParameters(
-		buildDir, baseConfigPath, &config, rpmsSources,
-		outputImageFormat, outputImageFile, packageSnapshotTime, toolsTar)
-	if err != nil {
-		return fmt.Errorf("invalid parameters:\n%w", err)
 	}
 
 	err = imagecustomizerlib.CheckEnvironmentVars()
@@ -100,119 +62,55 @@ func createNewImage(ctx context.Context, buildDir string, baseConfigPath string,
 	imagecustomizerlib.LogVersionsOfToolDeps()
 
 	// ensure build and output folders are created up front
-	err = os.MkdirAll(imageCreatorParameters.buildDirAbs, os.ModePerm)
+	err = os.MkdirAll(rc.BuildDirAbs, os.ModePerm)
 	if err != nil {
 		return err
 	}
 
-	err = os.MkdirAll(imageCreatorParameters.outputImageDir, os.ModePerm)
+	outputImageDir := filepath.Dir(rc.OutputImageFile)
+	err = os.MkdirAll(outputImageDir, os.ModePerm)
 	if err != nil {
 		return err
 	}
 
-	disks := imageCreatorParameters.config.Storage.Disks
+	disks := rc.Config.Storage.Disks
 	diskConfig := disks[0]
 	installOSFunc := func(imageChroot *safechroot.Chroot) error {
 		return nil
 	}
 
-	logger.Log.Infof("Creating new image with parameters: %+v\n", imageCreatorParameters)
+	logger.Log.Infof("Creating new image with parameters: %+v\n", rc)
 
 	// Create distro config from distro name and version
 	distroHandler := imagecustomizerlib.NewDistroHandler(distro, distroVersion)
 
 	partIdToPartUuid, err := imagecustomizerlib.CreateNewImage(
-		distroHandler.GetTargetOs(), imageCreatorParameters.rawImageFile,
-		diskConfig, imageCreatorParameters.config.Storage.FileSystems,
-		imageCreatorParameters.buildDirAbs, setupRoot, installOSFunc)
+		distroHandler.GetTargetOs(), rc.RawImageFile,
+		diskConfig, rc.Config.Storage.FileSystems,
+		rc.BuildDirAbs, setupRoot, installOSFunc)
 	if err != nil {
 		return err
 	}
 
 	logger.Log.Debugf("Part id to part uuid map %v\n", partIdToPartUuid)
-	logger.Log.Infof("Image UUID: %s", imageCreatorParameters.imageUuidStr)
+	logger.Log.Infof("Image UUID: %s", rc.ImageUuidStr)
 
-	partUuidToFstabEntry, osRelease, err := imagecustomizerlib.CustomizeImageHelperImageCreator(
-		ctx, imageCreatorParameters.buildDirAbs, imageCreatorParameters.configPath,
-		imageCreatorParameters.config, imageCreatorParameters.rawImageFile,
-		imageCreatorParameters.rpmsSources, false, imageCreatorParameters.imageUuidStr,
-		imageCreatorParameters.packageSnapshotTime, imageCreatorParameters.toolsTar, distroHandler)
+	partUuidToFstabEntry, osRelease, err := imagecustomizerlib.CustomizeImageHelperImageCreator(ctx, rc, toolsTar,
+		distroHandler)
 	if err != nil {
 		return err
 	}
 
-	imageCreatorParameters.partUuidToFstabEntry = partUuidToFstabEntry
-	imageCreatorParameters.osRelease = osRelease
 	logger.Log.Debugf("Part uuid to fstab entry: %v\n", partUuidToFstabEntry)
 	logger.Log.Debugf("OsRelease: %v\n", osRelease)
 
-	logger.Log.Infof("Writing: %s", imageCreatorParameters.outputImageFile)
+	logger.Log.Infof("Writing: %s", rc.OutputImageFile)
 
-	err = imagecustomizerlib.ConvertImageFile(imageCreatorParameters.rawImageFile,
-		imageCreatorParameters.outputImageFile, imageCreatorParameters.outputImageFormat)
+	err = imagecustomizerlib.ConvertImageFile(rc.RawImageFile, rc.OutputImageFile, rc.OutputImageFormat)
 	if err != nil {
 		return err
 	}
 	logger.Log.Infof("Success!")
 
 	return nil
-}
-
-func createImageCreatorParameters(buildDir string, configPath string, config *imagecustomizerapi.Config,
-	rpmsSources []string, outputImageFormat string, outputImageFile string, packageSnapshotTime string,
-	toolsTar string,
-) (*ImageCreatorParameters, error) {
-	ic := &ImageCreatorParameters{}
-
-	// working directories
-	buildDirAbs, err := filepath.Abs(buildDir)
-	if err != nil {
-		return nil, err
-	}
-
-	ic.buildDirAbs = buildDirAbs
-	ic.toolsTar = toolsTar
-
-	// intermediate writeable image
-	ic.rawImageFile = filepath.Join(buildDirAbs, imagecustomizerlib.BaseImageName)
-
-	// Create a uuid for the image
-	imageUuid, imageUuidStr, err := randomization.CreateUuid()
-	if err != nil {
-		return nil, err
-	}
-	ic.imageUuid = imageUuid
-	ic.imageUuidStr = imageUuidStr
-
-	// configuration
-	ic.configPath = configPath
-	ic.config = config
-
-	ic.rpmsSources = rpmsSources
-
-	err = imagecustomizerlib.ValidateRpmSources(rpmsSources)
-	if err != nil {
-		return nil, err
-	}
-
-	// output image
-	ic.outputImageFormat = imagecustomizerapi.ImageFormatType(outputImageFormat)
-	if err := ic.outputImageFormat.IsValid(); err != nil {
-		return nil, fmt.Errorf("invalid output image format:\n%w", err)
-	}
-
-	if ic.outputImageFormat == "" {
-		ic.outputImageFormat = config.Output.Image.Format
-	}
-
-	ic.outputImageFile = outputImageFile
-	if ic.outputImageFile == "" && config.Output.Image.Path != "" {
-		ic.outputImageFile = file.GetAbsPathWithBase(configPath, config.Output.Image.Path)
-	}
-
-	ic.outputImageBase = strings.TrimSuffix(filepath.Base(ic.outputImageFile), filepath.Ext(ic.outputImageFile))
-	ic.outputImageDir = filepath.Dir(ic.outputImageFile)
-	ic.packageSnapshotTime = packageSnapshotTime
-
-	return ic, nil
 }
