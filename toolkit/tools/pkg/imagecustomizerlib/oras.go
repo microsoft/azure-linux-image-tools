d6a61dfaba6dc8a7bec99c388da18d43a5ac5bac
Chris Gunn <chrisgun@microsoft.com>
2025-10-14 13:26:37 -0700

    WIP: Oras cache

--- toolkit/tools/pkg/imagecustomizerlib/oras.go
+++ toolkit/tools/pkg/imagecustomizerlib/oras.go
@@ -11,29 +11,50 @@ import (
 	"runtime"
 
 	"github.com/microsoft/azure-linux-image-tools/toolkit/tools/imagecustomizerapi"
+	"github.com/microsoft/azure-linux-image-tools/toolkit/tools/internal/file"
 	"github.com/microsoft/azure-linux-image-tools/toolkit/tools/internal/logger"
 	ociv1 "github.com/opencontainers/image-spec/specs-go/v1"
 	"oras.land/oras-go/v2"
-	"oras.land/oras-go/v2/content/file"
+	ocifile "oras.land/oras-go/v2/content/file"
+	"oras.land/oras-go/v2/content/oci"
 	"oras.land/oras-go/v2/registry/remote"
 )
 
-func downloadOciImage(ctx context.Context, oci imagecustomizerapi.OciImage, buildDir string) (string, string, error) {
-	repo, err := remote.NewRepository(oci.Uri)
-	if err != nil {
-		return "", "", fmt.Errorf("failed to open OCI repository (%s):\n%w", oci.Uri, err)
+func downloadOciImage(ctx context.Context, ociImage imagecustomizerapi.OciImage, buildDir string, imageCacheDir string,
+) (string, string, error) {
+	if imageCacheDir == "" {
+		return "", "", fmt.Errorf("image cache directory must be provided to download images")
 	}
 
-	tag := repo.Reference.Reference
+	imageCacheDirIsDir, err := file.IsDir(imageCacheDir)
+	if err != nil {
+		return "", "", fmt.Errorf("failed to locate image cache directory (%s):\n%w", imageCacheDir, err)
+	}
+	if !imageCacheDirIsDir {
+		return "", "", fmt.Errorf("image cache directory is not a directory (%s):\n%w", imageCacheDir, err)
+	}
+
+	ociCacheDirectory := filepath.Join(imageCacheDir, "oci")
+	cacheRepo, err := oci.NewWithContext(ctx, ociCacheDirectory)
+	if err != nil {
+		return "", "", fmt.Errorf("failed to open image cache directory (%s):\n%w", ociCacheDirectory, err)
+	}
+
+	remoteRepo, err := remote.NewRepository(ociImage.Uri)
+	if err != nil {
+		return "", "", fmt.Errorf("failed to open OCI repository (%s):\n%w", ociImage.Uri, err)
+	}
+
+	tag := remoteRepo.Reference.Reference
 
 	ociPlatform := (*ociv1.Platform)(nil)
-	if oci.Platform == nil {
+	if ociImage.Platform != nil {
 		ociPlatform = &ociv1.Platform{
-			OS:           oci.Platform.OS,
-			Architecture: oci.Platform.Architecture,
+			OS:           ociImage.Platform.OS,
+			Architecture: ociImage.Platform.Architecture,
 		}
 	} else {
-		descriptor, err := oras.Resolve(ctx, repo, tag, oras.DefaultResolveOptions)
+		descriptor, err := oras.Resolve(ctx, remoteRepo, tag, oras.DefaultResolveOptions)
 		if err != nil {
 			return "", "", fmt.Errorf("failed to retrieve OCI image artifact manifest:\n%w", err)
 		}
@@ -49,9 +70,16 @@ func downloadOciImage(ctx context.Context, oci imagecustomizerapi.OciImage, buil
 		}
 	}
 
-	copyOptions := oras.DefaultCopyOptions
-	copyOptions.WithTargetPlatform(ociPlatform)
-	copyOptions.PreCopy = func(ctx context.Context, desc ociv1.Descriptor) error {
+	resolveOptions := oras.DefaultResolveOptions
+	resolveOptions.TargetPlatform = ociPlatform
+
+	descriptor, err := oras.Resolve(ctx, remoteRepo, tag, resolveOptions)
+	if err != nil {
+		return "", "", fmt.Errorf("failed to retrieve OCI image artifact manifest:\n%w", err)
+	}
+
+	copyGraphOptions := oras.DefaultCopyGraphOptions
+	copyGraphOptions.PreCopy = func(ctx context.Context, desc ociv1.Descriptor) error {
 		title, hasTitle := desc.Annotations[ociv1.AnnotationTitle]
 		if hasTitle {
 			logger.Log.Debugf("Downloading OCI file (%s)", title)
@@ -60,20 +88,34 @@ func downloadOciImage(ctx context.Context, oci imagecustomizerapi.OciImage, buil
 		return nil
 	}
 
+	err = oras.CopyGraph(ctx, remoteRepo, cacheRepo, descriptor, copyGraphOptions)
+	if err != nil {
+		return "", "", fmt.Errorf("failed to download OCI image artifact:\n%w", err)
+	}
+
 	destinationDirPath, err := os.MkdirTemp(buildDir, "oci-image-")
 	if err != nil {
 		return "", "", fmt.Errorf("failed to create OCI download directory:\n%w", err)
 	}
 
-	fs, err := file.New(destinationDirPath)
+	fs, err := ocifile.New(destinationDirPath)
 	if err != nil {
 		return "", "", fmt.Errorf("failed to initialize OCI download directory (%s):\n%w", destinationDirPath, err)
 	}
 	defer fs.Close()
 
-	_, err = oras.Copy(ctx, repo, tag, fs, tag, copyOptions)
+	copyGraphOptions.PreCopy = func(ctx context.Context, desc ociv1.Descriptor) error {
+		title, hasTitle := desc.Annotations[ociv1.AnnotationTitle]
+		if hasTitle {
+			logger.Log.Debugf("Staging OCI file (%s)", title)
+		}
+
+		return nil
+	}
+
+	err = oras.CopyGraph(ctx, cacheRepo, fs, descriptor, copyGraphOptions)
 	if err != nil {
-		return "", "", fmt.Errorf("failed to download OCI image artifact:\n%w", err)
+		return "", "", fmt.Errorf("failed to stage OCI image artifact:\n%w", err)
 	}
 
 	err = fs.Close()
@@ -100,11 +142,13 @@ func downloadOciImage(ctx context.Context, oci imagecustomizerapi.OciImage, buil
 	}
 
 	if len(imageFilePaths) <= 0 {
-		return "", "", fmt.Errorf("no image files (*.vhdx, *.vhd, *.qcow2, *.img, *.raw) found in OCI artifact:\n%w", err)
+		return "", "", fmt.Errorf("no image files (*.vhdx, *.vhd, *.qcow2, *.img, *.raw) found in OCI artifact")
 	}
 
 	if len(imageFilePaths) > 1 {
-		return "", "", fmt.Errorf("too many image files (*.vhdx, *.vhd, *.qcow2, *.img, *.raw) found in OCI artifact (count=%d):\n%w", len(imageFilePaths), err)
+		err = fmt.Errorf("too many image files (*.vhdx, *.vhd, *.qcow2, *.img, *.raw) found in OCI artifact (count=%d)",
+			len(imageFilePaths))
+		return "", "", err
 	}
 
 	imageFilePath := imageFilePaths[0]
