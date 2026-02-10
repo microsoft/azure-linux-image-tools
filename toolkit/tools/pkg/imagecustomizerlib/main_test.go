695d080dadb918ed34e2789ca1e56cc8e77abead
Vince Perri <5596945+vinceaperri@users.noreply.github.com>
2026-02-10 20:21:36 +0000

    Add Ubuntu 22/24.04 Azure cloud test base images: - New flags: --base-image-azure-cloud-ubuntu2204 and --base-image-azure-cloud-ubuntu2404 - New functions: findFirstAvailableImage, for code reuse, and checkSkipForCustomizeDefaultImages, returning one default image for each distro, skipping on no availability. - New constants: paramBaseImageCoreEfiAzl2, paramBaseImageCoreEfiAzl3, paramBaseImageBareMetalAzl2, paramBaseImageBareMetalAzl3, paramBaseImageAzureCloudUbuntu2204, paramBaseImageAzureCloudUbuntu2404, paramLogLevel

--- toolkit/tools/pkg/imagecustomizerlib/main_test.go
+++ toolkit/tools/pkg/imagecustomizerlib/main_test.go
@@ -16,23 +16,41 @@ import (
 
 const (
 	baseImageDistroAzureLinux = "azurelinux"
+	baseImageDistroUbuntu     = "ubuntu"
 
 	// Azure Linux versions
 	baseImageVersionAzl2 = "2.0"
 	baseImageVersionAzl3 = "3.0"
 
+	// Ubuntu versions
+	baseImageVersionUbuntu2204 = "22.04"
+	baseImageVersionUbuntu2404 = "24.04"
+
 	// Azure Linux variants
 	baseImageAzureLinuxVariantCoreEfi   = "core-efi"
 	baseImageAzureLinuxVariantBareMetal = "bare-metal"
+
+	// Ubuntu variants
+	baseImageVariantUbuntuAzureCloud = "azure-cloud"
+
+	// Flag names
+	paramBaseImageCoreEfiAzl2          = "base-image-core-efi-azl2"
+	paramBaseImageCoreEfiAzl3          = "base-image-core-efi-azl3"
+	paramBaseImageBareMetalAzl2        = "base-image-bare-metal-azl2"
+	paramBaseImageBareMetalAzl3        = "base-image-bare-metal-azl3"
+	paramBaseImageAzureCloudUbuntu2204 = "base-image-azure-cloud-ubuntu2204"
+	paramBaseImageAzureCloudUbuntu2404 = "base-image-azure-cloud-ubuntu2404"
+	paramLogLevel                      = "log-level"
 )
 
 type testBaseImageInfo struct {
-	Name      string
-	Distro    string
-	Version   string
-	Variant   string
-	ParamName string
-	Param     *string
+	Name        string
+	Distro      string
+	Version     string
+	Variant     string
+	ParamName   string
+	Param       *string
+	MountPoints []testutils.MountPoint
 }
 
 var (
@@ -57,40 +75,77 @@ var (
 		},
 	}
 
+	ubuntuAzureCloudMountPoints = []testutils.MountPoint{
+		{
+			PartitionNum:   1,
+			Path:           "/",
+			FileSystemType: "ext4",
+		},
+		{
+			PartitionNum:   15,
+			Path:           "/boot/efi",
+			FileSystemType: "vfat",
+		},
+	}
+
 	testBaseImageAzl2CoreEfi = testBaseImageInfo{
-		Name:      "AzureLinux2CoreEfi",
-		Distro:    baseImageDistroAzureLinux,
-		Version:   baseImageVersionAzl2,
-		Variant:   baseImageAzureLinuxVariantCoreEfi,
-		ParamName: "base-image-core-efi-azl2",
-		Param:     baseImageCoreEfiAzl2,
+		Name:        "AzureLinux2CoreEfi",
+		Distro:      baseImageDistroAzureLinux,
+		Version:     baseImageVersionAzl2,
+		Variant:     baseImageAzureLinuxVariantCoreEfi,
+		ParamName:   paramBaseImageCoreEfiAzl2,
+		Param:       baseImageCoreEfiAzl2,
+		MountPoints: azureLinuxCoreEfiMountPoints,
 	}
 
 	testBaseImageAzl3CoreEfi = testBaseImageInfo{
-		Name:      "AzureLinux3CoreEfi",
-		Distro:    baseImageDistroAzureLinux,
-		Version:   baseImageVersionAzl3,
-		Variant:   baseImageAzureLinuxVariantCoreEfi,
-		ParamName: "base-image-core-efi-azl3",
-		Param:     baseImageCoreEfiAzl3,
+		Name:        "AzureLinux3CoreEfi",
+		Distro:      baseImageDistroAzureLinux,
+		Version:     baseImageVersionAzl3,
+		Variant:     baseImageAzureLinuxVariantCoreEfi,
+		ParamName:   paramBaseImageCoreEfiAzl3,
+		Param:       baseImageCoreEfiAzl3,
+		MountPoints: azureLinuxCoreEfiMountPoints,
 	}
 
 	testBaseImageAzl2BareMetal = testBaseImageInfo{
-		Name:      "AzureLinux2BareMetal",
-		Distro:    baseImageDistroAzureLinux,
-		Version:   baseImageVersionAzl2,
-		Variant:   baseImageAzureLinuxVariantBareMetal,
-		ParamName: "base-image-bare-metal-azl2",
-		Param:     baseImageBareMetalAzl2,
+		Name:        "AzureLinux2BareMetal",
+		Distro:      baseImageDistroAzureLinux,
+		Version:     baseImageVersionAzl2,
+		Variant:     baseImageAzureLinuxVariantBareMetal,
+		ParamName:   paramBaseImageBareMetalAzl2,
+		Param:       baseImageBareMetalAzl2,
+		MountPoints: azureLinuxBaremetalMountPoints,
 	}
 
 	testBaseImageAzl3BareMetal = testBaseImageInfo{
-		Name:      "AzureLinux3BareMetal",
-		Distro:    baseImageDistroAzureLinux,
-		Version:   baseImageVersionAzl3,
-		Variant:   baseImageAzureLinuxVariantBareMetal,
-		ParamName: "base-image-bare-metal-azl3",
-		Param:     baseImageBareMetalAzl3,
+		Name:        "AzureLinux3BareMetal",
+		Distro:      baseImageDistroAzureLinux,
+		Version:     baseImageVersionAzl3,
+		Variant:     baseImageAzureLinuxVariantBareMetal,
+		ParamName:   paramBaseImageBareMetalAzl3,
+		Param:       baseImageBareMetalAzl3,
+		MountPoints: azureLinuxBaremetalMountPoints,
+	}
+
+	testBaseImageUbuntu2204AzureCloud = testBaseImageInfo{
+		Name:        "Ubuntu2204AzureCloud",
+		Distro:      baseImageDistroUbuntu,
+		Version:     baseImageVersionUbuntu2204,
+		Variant:     baseImageVariantUbuntuAzureCloud,
+		ParamName:   paramBaseImageAzureCloudUbuntu2204,
+		Param:       baseImageUbuntuAzureCloud2204,
+		MountPoints: ubuntuAzureCloudMountPoints,
+	}
+
+	testBaseImageUbuntu2404AzureCloud = testBaseImageInfo{
+		Name:        "Ubuntu2404AzureCloud",
+		Distro:      baseImageDistroUbuntu,
+		Version:     baseImageVersionUbuntu2404,
+		Variant:     baseImageVariantUbuntuAzureCloud,
+		ParamName:   paramBaseImageAzureCloudUbuntu2404,
+		Param:       baseImageUbuntuAzureCloud2404,
+		MountPoints: ubuntuAzureCloudMountPoints,
 	}
 
 	baseImageAzureLinuxAll = []testBaseImageInfo{
@@ -106,14 +161,21 @@ var (
 		testBaseImageAzl2CoreEfi,
 		testBaseImageAzl2BareMetal,
 	}
+
+	defaultUbuntuPriorityList = []testBaseImageInfo{
+		testBaseImageUbuntu2404AzureCloud,
+		testBaseImageUbuntu2204AzureCloud,
+	}
 )
 
 var (
-	baseImageCoreEfiAzl2   = flag.String("base-image-core-efi-azl2", "", "An Azure Linux 2.0 core-efi image to use as a base image.")
-	baseImageCoreEfiAzl3   = flag.String("base-image-core-efi-azl3", "", "An Azure Linux 3.0 core-efi image to use as a base image.")
-	baseImageBareMetalAzl2 = flag.String("base-image-bare-metal-azl2", "", "An Azure Linux 2.0 bare-metal image to use as a base image.")
-	baseImageBareMetalAzl3 = flag.String("base-image-bare-metal-azl3", "", "An Azure Linux 3.0 bare-metal image to use as a base image.")
-	logLevel               = flag.String("log-level", "info", "The log level (error, warning, info, debug, or trace)")
+	baseImageCoreEfiAzl2          = flag.String(paramBaseImageCoreEfiAzl2, "", "An Azure Linux 2.0 core-efi image to use as a base image.")
+	baseImageCoreEfiAzl3          = flag.String(paramBaseImageCoreEfiAzl3, "", "An Azure Linux 3.0 core-efi image to use as a base image.")
+	baseImageBareMetalAzl2        = flag.String(paramBaseImageBareMetalAzl2, "", "An Azure Linux 2.0 bare-metal image to use as a base image.")
+	baseImageBareMetalAzl3        = flag.String(paramBaseImageBareMetalAzl3, "", "An Azure Linux 3.0 bare-metal image to use as a base image.")
+	baseImageUbuntuAzureCloud2204 = flag.String(paramBaseImageAzureCloudUbuntu2204, "", "An Ubuntu 22.04 Azure cloud image to use as a base image.")
+	baseImageUbuntuAzureCloud2404 = flag.String(paramBaseImageAzureCloudUbuntu2404, "", "An Ubuntu 24.04 Azure cloud image to use as a base image.")
+	logLevel                      = flag.String(paramLogLevel, "info", "The log level (error, warning, info, debug, or trace)")
 )
 
 var (
@@ -180,15 +242,43 @@ func checkSkipForCustomizeImage(t *testing.T, baseImage testBaseImageInfo) strin
 	return *baseImage.Param
 }
 
-func checkSkipForCustomizeDefaultAzureLinuxImage(t *testing.T) (string, testBaseImageInfo) {
-	testutils.CheckSkipForCustomizeImageRequirements(t)
-
-	for _, imageInfo := range defaultAzureLinuxPriorityList {
+func findFirstAvailableImage(priorityList []testBaseImageInfo) (testBaseImageInfo, bool) {
+	for _, imageInfo := range priorityList {
 		if imageInfo.Param != nil && *imageInfo.Param != "" {
-			return *imageInfo.Param, imageInfo
+			return imageInfo, true
 		}
 	}
 
-	t.Skipf("--%s is required for this test", defaultAzureLinuxPriorityList[0].ParamName)
-	return "", testBaseImageInfo{}
+	return testBaseImageInfo{}, false
+}
+
+func checkSkipForCustomizeDefaultAzureLinuxImage(t *testing.T) (string, testBaseImageInfo) {
+	testutils.CheckSkipForCustomizeImageRequirements(t)
+
+	imageInfo, found := findFirstAvailableImage(defaultAzureLinuxPriorityList)
+	if !found {
+		t.Skipf("--%s is required for this test", defaultAzureLinuxPriorityList[0].ParamName)
+	}
+
+	return *imageInfo.Param, imageInfo
+}
+
+func checkSkipForCustomizeDefaultImages(t *testing.T) []testBaseImageInfo {
+	testutils.CheckSkipForCustomizeImageRequirements(t)
+
+	var images []testBaseImageInfo
+
+	if imageInfo, found := findFirstAvailableImage(defaultAzureLinuxPriorityList); found {
+		images = append(images, imageInfo)
+	}
+
+	if imageInfo, found := findFirstAvailableImage(defaultUbuntuPriorityList); found {
+		images = append(images, imageInfo)
+	}
+
+	if len(images) == 0 {
+		t.Skipf("--%s is required for this test", defaultAzureLinuxPriorityList[0].ParamName)
+	}
+
+	return images
 }
