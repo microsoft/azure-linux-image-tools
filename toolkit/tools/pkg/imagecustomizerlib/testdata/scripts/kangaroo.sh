e132e56c080fde6a3f8792870dbe6a0d58a13af7
Vince Perri <5596945+vinceaperri@users.noreply.github.com>
2026-02-10 22:36:35 +0000

    Fix TestCustomizeImageRunScripts test for Ubuntu

--- toolkit/tools/pkg/imagecustomizerlib/testdata/scripts/kangaroo.sh
+++ toolkit/tools/pkg/imagecustomizerlib/testdata/scripts/kangaroo.sh
@@ -2,6 +2,7 @@ set -eux
 echo "Kangaroo" | tee --append /log.txt
 echo "Working dir: $(pwd)" | tee --append /log.txt
 
-# Ensure files in the config's directory as accessible to scripts.
-SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+# Ensure files in the config's directory are accessible to scripts.
+# Use $0 instead of ${BASH_SOURCE[0]} for POSIX compatibility (dash on Ubuntu).
+SCRIPT_DIR=$(dirname "$0")
 cp "$SCRIPT_DIR/../files/a.txt" /
