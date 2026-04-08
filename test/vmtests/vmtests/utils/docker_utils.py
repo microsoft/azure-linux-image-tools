67d7a12f84bf014a577785e8bbfc2c965dfe99b0
Chris Gunn <chrisgun@microsoft.com>
2026-04-08 16:15:21 -0700

    Add --log-format test

--- test/vmtests/vmtests/utils/docker_utils.py
+++ test/vmtests/vmtests/utils/docker_utils.py
@@ -2,11 +2,12 @@
 # Licensed under the MIT License.
 
 import logging
-from typing import Any
+from concurrent.futures import ThreadPoolExecutor
+from typing import Any, List, Tuple
 
-import docker
 from docker import DockerClient
 from docker.models.containers import Container
+from docker.types.daemon import CancellableStream
 
 
 # Can be used with a `with` statement for deleting a Docker container.
@@ -25,17 +26,20 @@ class ContainerRemove:
 
 
 # Run a container, log its stdout and stderr, and wait for it to complete.
-def container_run(docker_client: DockerClient, *args: Any, **kwargs: Any) -> "docker._types.WaitContainerResponse":
+def container_run(docker_client: DockerClient, *args: Any, **kwargs: Any) -> Tuple[List[str], List[str]]:
     with ContainerRemove(docker_client.containers.run(*args, **kwargs)) as container:
         return container_log_and_wait(container.container)
 
 
 # Waits for a docker container to exit, while logging stdout and stderr.
-def container_log_and_wait(container: Container) -> "docker._types.WaitContainerResponse":
+def container_log_and_wait(container: Container) -> Tuple[List[str], List[str]]:
     # Log stdout and stderr.
-    logs = container.logs(stdout=True, stderr=True, stream=True)
-    for log in logs:
-        logging.debug(log.decode("utf-8", errors="replace").rstrip())
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        stdout_work = executor.submit(_process_logs, container.logs(stdout=True, stream=True))
+        stderr_work = executor.submit(_process_logs, container.logs(stderr=True, stream=True))
+
+        stdout_lines = stdout_work.result()
+        stderr_lines = stderr_work.result()
 
     # Wait for the container to close.
     result = container.wait()
@@ -44,4 +48,15 @@ def container_log_and_wait(container: Container) -> "docker._types.WaitContainer
     if exit_code != 0:
         raise Exception(f"Container failed with {exit_code}")
 
-    return result
+    return (stdout_lines, stderr_lines)
+
+
+def _process_logs(logs: CancellableStream[bytes]) -> List[str]:
+    lines = []
+
+    for log in logs:
+        log_str = log.decode("utf-8", errors="replace")
+        lines.append(log_str)
+        logging.debug(log_str.rstrip())
+
+    return lines
