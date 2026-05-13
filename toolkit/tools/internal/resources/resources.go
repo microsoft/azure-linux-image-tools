bb1b031dc0b0f3bd92779efea6d513ad111aa285
Vince Perri <5596945+vinceaperri@users.noreply.github.com>
2026-05-13 18:00:11 +0000

    reorganize assets

--- toolkit/tools/internal/resources/resources.go
+++ toolkit/tools/internal/resources/resources.go
@@ -9,11 +9,13 @@ import (
 
 const (
 	// Assets
-	AssetsGrubCfgFile      = "assets/grub2/grub.cfg"
-	AssetsGrubDefFileAzl3  = "assets/grub2/grub-azl3"
-	AssetsGrubDefFileAzl4  = "assets/grub2/grub-azl4"
-	AssetsGrubStubFileAzl3 = "assets/efi/grub/grub-azl3.cfg"
-	AssetsGrubStubFileAzl4 = "assets/efi/grub/grub-azl4.cfg"
+	AssetsGrubCfgFile = "assets/grub2/grub.cfg"
+
+	AssetsGrubDefFileAzl3  = "assets/azurelinux/azurelinux-3.0/grub2/grub"
+	AssetsGrubStubFileAzl3 = "assets/azurelinux/azurelinux-3.0/efi/grub/grub.cfg"
+
+	AssetsGrubDefFileAzl4  = "assets/azurelinux/azurelinux-4.0/grub2/grub"
+	AssetsGrubStubFileAzl4 = "assets/azurelinux/azurelinux-4.0/efi/grub/grub.cfg"
 
 	// Verity Signature Module Files
 	VerityMountBootPartitionSetupFile     = "verity-signature/90mountbootpartition/module-setup.sh"
